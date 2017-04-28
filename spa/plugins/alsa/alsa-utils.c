308acaf0ad8d63d10e9e2a2f83a04dfb0ddf8385
Wim Taymans <wtaymans@redhat.com>
2017-04-28 12:12:14 +0200

    alsa: fix offset

--- spa/plugins/alsa/alsa-utils.c
+++ spa/plugins/alsa/alsa-utils.c
@@ -377,7 +377,7 @@ pull_frames (SpaALSAState *state,
       reuse = avail == n_bytes;
     } else {
       offs = SPA_MIN (d[0].chunk->offset + state->ready_offset, d[0].maxsize);
-      size = SPA_MIN (d[0].chunk->size, d[0].maxsize - offs);
+      size = SPA_MIN (d[0].chunk->size, d[0].maxsize) - offs;
       src = SPA_MEMBER (d[0].data, offs, uint8_t);
 
       n_bytes = SPA_MIN (size, to_write * state->frame_size);

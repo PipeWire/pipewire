0b7c3e74074807ad06a19a84b2a930615ad1bdfd
Wim Taymans <wtaymans@redhat.com>
2017-12-18 13:14:18 +0100

    fix spa_support_find

--- spa/include/spa/support/plugin.h
+++ spa/include/spa/support/plugin.h
@@ -85,9 +85,10 @@ static inline void *spa_support_find(const struct spa_support *support,
 				     const char *type)
 {
 	uint32_t i;
-	for (i = 0; i < n_support; i++)
-		if (strcmp(support->type, type) == 0)
-			return support->data;
+	for (i = 0; i < n_support; i++) {
+		if (strcmp(support[i].type, type) == 0)
+			return support[i].data;
+	}
 	return NULL;
 }
 

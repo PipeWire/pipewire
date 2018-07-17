36d883b8c87310913e8149b15e3aa8df0274d53a
Wim Taymans <wtaymans@redhat.com>
2018-07-17 10:31:17 +0200

    version: add version file

--- src/pipewire/pipewire.c
+++ src/pipewire/pipewire.c
@@ -31,8 +31,9 @@
 
 #include <spa/support/dbus.h>
 
-#include "pipewire/pipewire.h"
-#include "pipewire/private.h"
+#include "pipewire.h"
+#include "private.h"
+#include "version.h"
 
 static char **categories = NULL;
 
@@ -377,4 +378,10 @@ enum pw_direction pw_direction_reverse(enum pw_direction direction)
 	else if (direction == PW_DIRECTION_OUTPUT)
 		return PW_DIRECTION_INPUT;
 	return direction;
+}
+
+/** Get the currently running version */
+const char* pw_get_library_version(void)
+{
+	return pw_get_headers_version();
 }

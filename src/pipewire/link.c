8f8ed7270a02b43377b1281367d800a818069d40
Wim Taymans <wtaymans@redhat.com>
2018-08-09 10:59:03 +0200

    link: reset port state on deactivate
    Make sure we renegotiate the mix ports when we deactivate the link.

--- src/pipewire/link.c
+++ src/pipewire/link.c
@@ -1083,6 +1083,7 @@ int pw_link_deactivate(struct pw_link *this)
 		pw_log_debug("port %p: state %d -> %d", this->input,
 				this->input->state, PW_PORT_STATE_PAUSED);
 	}
+	this->rt.in_mix.state = PW_PORT_STATE_CONFIGURE;
 
 	if (output_node->n_used_input_links <= output_node->idle_used_input_links &&
 	    output_node->n_used_output_links <= output_node->idle_used_output_links &&
@@ -1091,6 +1092,7 @@ int pw_link_deactivate(struct pw_link *this)
 		pw_log_debug("port %p: state %d -> %d", this->output,
 				this->output->state, PW_PORT_STATE_PAUSED);
 	}
+	this->rt.out_mix.state = PW_PORT_STATE_CONFIGURE;
 
 	pw_link_update_state(this, PW_LINK_STATE_INIT, NULL);
 

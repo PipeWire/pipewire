cb3ccb7001d26afb0017524034d1bd65f6947135
Wim Taymans <wtaymans@redhat.com>
2018-08-28 19:56:58 +0200

    control: add missing file

---
+++ spa/include/spa/control/control.h
@@ -0,0 +1,44 @@
+/* Simple Plugin API
+ * Copyright (C) 2018 Wim Taymans <wim.taymans@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef __SPA_CONTROL_H__
+#define __SPA_CONTROL_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <spa/utils/defs.h>
+#include <spa/pod/pod.h>
+
+/** Controls
+ *
+ */
+
+/** Different Control types */
+enum spa_control_type {
+	SPA_CONTROL_properties,
+	SPA_CONTROL_midi,
+};
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+
+#endif /* __SPA_CONTROL_H__ */

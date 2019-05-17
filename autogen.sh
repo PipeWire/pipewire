ff44d5742611963a73d9647ef97d2abf8e1640a8
Thomas Zimmermann <tzimmermann@suse.de>
2019-05-17 10:22:57 +0200

    autogen.sh: Check for meson
    If meson is not installed, at least an error message should be printed.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,17 @@
 git submodule init
 git submodule update
 
+if [ -z $MESON ]; then
+	MESON=`which meson`
+fi
+if [ -z $MESON ]; then
+	echo "error: Meson not found."
+	echo "Install meson to configure and build Pipewire. If meson" \
+	     "is already installed, set the environment variable MESON" \
+	     "to the binary's path."
+	exit 1;
+fi
+
 mkdir -p build
-meson setup "$@" build  # use 'autogen.sh --reconfigure' to update
+$MESON setup "$@" build # use 'autogen.sh --reconfigure' to update
 ln -sf build/Makefile Makefile

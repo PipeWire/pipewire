99169c5e8e41f75273a5444011924a497bf79a18
Thomas Zimmermann <tzimmermann@suse.de>
2019-05-17 10:18:47 +0200

    autogen.sh: Put meson arguments right after command name
    According to the meson man page, arguments go directly after the command's
    name. Rearrange the call accordingly.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>

--- autogen.sh
+++ autogen.sh
@@ -18,5 +18,5 @@
 # Only there to make jhbuild happy
 
 mkdir -p build
-meson setup build "$@"  # use 'autogen.sh --reconfigure' to update
+meson setup "$@" build  # use 'autogen.sh --reconfigure' to update
 ln -sf build/Makefile Makefile

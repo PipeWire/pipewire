203c89daaf1dbba3be79a99161d4085011fb1753
Wim Taymans <wtaymans@redhat.com>
2020-06-02 18:16:12 +0200

    pw-cat: cleanup registry proxy

--- src/tools/pw-cat.c
+++ src/tools/pw-cat.c
@@ -1583,6 +1583,8 @@ error_connect_fail:
 	if (data.stream)
 		pw_stream_destroy(data.stream);
 error_no_stream:
+	if (data.registry)
+		pw_proxy_destroy((struct pw_proxy*)data.registry);
 error_no_registry:
 	pw_core_disconnect(data.core);
 error_ctx_connect_failed:
@@ -1598,6 +1600,7 @@ error_bad_file:
 		sf_close(data.file);
 	if (data.midi.file)
 		midi_file_close(data.midi.file);
+	pw_deinit();
 	return exit_code;
 
 error_usage:

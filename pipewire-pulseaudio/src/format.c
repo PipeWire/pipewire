17b6dd40e85a05000e497d2196d1ba6662cc938c
Alexander Volkov <avolkov@astralinux.ru>
2020-09-29 16:59:38 +0300

    pulse: fix build with pulseaudio < 13.0

--- pipewire-pulseaudio/src/format.c
+++ pipewire-pulseaudio/src/format.c
@@ -76,8 +76,10 @@ static const char* const _encoding_str_table[]= {
     [PA_ENCODING_MPEG_IEC61937] = "mpeg-iec61937",
     [PA_ENCODING_DTS_IEC61937] = "dts-iec61937",
     [PA_ENCODING_MPEG2_AAC_IEC61937] = "mpeg2-aac-iec61937",
+#if PA_CHECK_VERSION(13, 0, 0)
     [PA_ENCODING_TRUEHD_IEC61937] = "truehd-iec61937",
     [PA_ENCODING_DTSHD_IEC61937] = "dtshd-iec61937",
+#endif
     [PA_ENCODING_ANY] = "any",
 };
 

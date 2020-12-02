4c486c7ae322fbcc903884a30da72552a1b57f07
Wim Taymans <wtaymans@redhat.com>
2020-12-02 11:47:08 +0100

    spa: remove media.class from null-audio-sink
    We can use it with othe media.class values as well

--- spa/plugins/support/null-audio-sink.c
+++ spa/plugins/support/null-audio-sink.c
@@ -286,7 +286,6 @@ static int impl_node_send_command(void *object, const struct spa_command *comman
 }
 
 static const struct spa_dict_item node_info_items[] = {
-	{ SPA_KEY_MEDIA_CLASS, "Audio/Sink" },
 	{ SPA_KEY_NODE_DRIVER, "true" },
 };
 

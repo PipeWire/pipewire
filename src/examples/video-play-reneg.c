e871b761fea7e8a8154a3b0e5b6f948a9de91b12
columbarius <co1umbarius@protonmail.com>
2020-12-07 15:08:32 +0100

    examples: only announce SPA_DATA_MemPtr

--- src/examples/video-play-reneg.c
+++ src/examples/video-play-reneg.c
@@ -244,7 +244,7 @@ on_stream_param_changed(void *_data, uint32_t id, const struct spa_pod *param)
 		SPA_PARAM_BUFFERS_size,    SPA_POD_Int(data->stride * data->size.height),
 		SPA_PARAM_BUFFERS_stride,  SPA_POD_Int(data->stride),
 		SPA_PARAM_BUFFERS_align,   SPA_POD_Int(16),
-		SPA_PARAM_BUFFERS_dataType, SPA_POD_CHOICE_FLAGS_Int((1<<SPA_DATA_MemFd) | (1<<SPA_DATA_MemPtr)));
+		SPA_PARAM_BUFFERS_dataType, SPA_POD_CHOICE_FLAGS_Int((1<<SPA_DATA_MemPtr)));
 
 	/* we are done */
 	pw_stream_update_params(stream, params, 1);

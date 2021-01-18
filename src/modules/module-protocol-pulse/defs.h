6b2ec6d619497a344b80b7e65dc7772af7de0ca4
Wim Taymans <wtaymans@redhat.com>
2021-01-18 16:24:00 +0100

    pulse-server: require at least 32 samples per buffer

--- src/modules/module-protocol-pulse/defs.h
+++ src/modules/module-protocol-pulse/defs.h
@@ -48,7 +48,7 @@
 #define MAX_BUFFERS     64u
 
 #define MIN_BLOCK	64u
-#define MIN_SAMPLES	16u
+#define MIN_SAMPLES	32u
 #define MIN_USEC	(MIN_SAMPLES * SPA_USEC_PER_SEC / 48000u)
 
 #define MAXLENGTH		(4*1024*1024) /* 4MB */

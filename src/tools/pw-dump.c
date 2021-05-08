PipeWire/pipewire

commit af63d084536f65efeb294d79fed0403cc1cee664

tools/pw-dump: only print colors if we're connected to a terminal
We don't want ansi escape codes in the output of `pw-dump > log`. And where
colors are needed after the fact, it's easy to just run `jq . $file`.

src/tools/pw-dump.c
@@ -1429,7 +1429,7 @@ int main(int argc, char *argv[])
 	pw_init(&argc, &argv);
 
 	data.out = stdout;
-	if (getenv("NO_COLOR") == NULL)
+	if (isatty(fileno(data.out) && getenv("NO_COLOR") == NULL))
 		colors = true;
 
 	while ((c = getopt_long(argc, argv, "hVr:mN", long_options, NULL)) != -1) {

commit 68f75bbd57b3b1fce7efa7b95c64d05f20eca28e

tools/pw-dump: fix parentheses around `isatty()` call
Previously,

  isatty(fileno(data.out) && getenv("NO_COLOR") == NULL))

would call `isatty()` with

  fileno(data.out) && getenv("NO_COLOR") == NULL

as its argument. This meant that, for example,

  NO_COLOR=1 pw-dump

would still produce colored output when run with
a TTY as its standard input.

Fix that by moving the parenthesis.

Fixes: af63d084536f ("tools/pw-dump: only print colors if we're connected to a terminal")

src/tools/pw-dump.c
@@ -1429,7 +1429,7 @@ int main(int argc, char *argv[])
 	pw_init(&argc, &argv);
 
 	data.out = stdout;
-	if (isatty(fileno(data.out) && getenv("NO_COLOR") == NULL))
+	if (isatty(fileno(data.out)) && getenv("NO_COLOR") == NULL)
 		colors = true;
 
 	while ((c = getopt_long(argc, argv, "hVr:mN", long_options, NULL)) != -1) {
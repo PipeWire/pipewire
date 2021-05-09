c3b87ea4b215a4e4e2d9b3cb74a23918d717e994
Peter Hutterer <peter.hutterer@who-t.net>
2021-05-10 08:35:00 +1000

    pw-uninstalled: drop handling of VERSION
    This functionality was removed in 08543384f101e95d07eb041e074fe9e5d009c2e2

--- pw-uninstalled.sh
+++ pw-uninstalled.sh
@@ -4,13 +4,10 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-while getopts ":b:v:" opt; do
+while getopts ":b:" opt; do
 	case ${opt} in
 		b)
 			BUILDDIR=${OPTARG}
-			;;
-		v)
-			VERSION=${OPTARG}
 			;;
 		\?)
 			echo "Invalid option: -${OPTARG}"

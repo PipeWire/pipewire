b64f0d581f720943ceed5e8285319cc5d44e47e1
Peter Hutterer <peter.hutterer@who-t.net>
2021-05-25 19:55:06 +1000

    doc: switch from .md to .dox files and impose some general structure
    This is an attempt of breaking up the documentation, currently spread across
    several pages. We're left with a few high-level topics with various things
    grouped underneath those.
    
    Further refinement is necessary, but this can now be done in incremental steps
    over massive reworks.

--- src/pipewire/pipewire.h
+++ src/pipewire/pipewire.h
@@ -60,55 +60,6 @@ extern "C" {
 #include <pipewire/utils.h>
 #include <pipewire/version.h>
 
-/** \mainpage
- *
- * \section sec_intro Introduction
- *
- * This document describes the API for the PipeWire multimedia framework.
- * The API consists of two parts:
- *
- * \li The core API to access a PipeWire instance.
- * (See \subpage page_core_api)
- * \li The implementation API and tools to build new objects and
- * modules (See \subpage page_implementation_api)
- *
- * \section sec_errors Error reporting
- *
- * Functions return either NULL with errno set or a negative int error
- * code when an error occurs. Error codes are used from the SPA plugin
- * library on which PipeWire is built.
- *
- * Some functions might return asynchronously. The error code for such
- * functions is positive and SPA_RESULT_IS_ASYNC() will return true.
- * SPA_RESULT_ASYNC_SEQ() can be used to get the unique sequence number
- * associated with the async operation.
- *
- * The object returning the async result code will have some way to
- * signal the completion of the async operation (with, for example, a
- * callback). The sequence number can be used to see which operation
- * completed.
- *
- * \section sec_logging Logging
- *
- * The `PIPEWIRE_DEBUG` environment variable can be used to enable
- * more debugging. The format is:
- *
- * `<level>[<category>;...]`
- *
- * - `<level>` specifies the log level:
- *   + `0`: no logging is enabled
- *   + `1`: Error logging is enabled
- *   + `2`: Warnings are enabled
- *   + `3`: Informational messages are enabled
- *   + `4`: Debug messages are enabled
- *   + `5`: Trace messages are enabled. These messages can be logged
- *		from the realtime threads.
- *
- * - `<category>`:  Specifies a string category to enable. Many categories
- *		  can be separated by commas. Current categories are:
- *   + `connection`: to log connection messages
- */
-
 /** \defgroup pw_pipewire PipeWire initialization and infrastructure functions
  */
 

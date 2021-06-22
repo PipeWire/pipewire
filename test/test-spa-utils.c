575d4456e1166d022a6068bf34995bfdb31eb1e8
Wim Taymans <wtaymans@redhat.com>
2021-06-22 16:31:51 +0200

    tests: fix test

--- test/test-spa-utils.c
+++ test/test-spa-utils.c
@@ -143,7 +143,8 @@ PWTEST(utils_abi)
 	pwtest_int_eq(SPA_TYPE_OBJECT_ParamRoute, 0x40009);
 	pwtest_int_eq(SPA_TYPE_OBJECT_Profiler, 0x4000a);
 	pwtest_int_eq(SPA_TYPE_OBJECT_ParamLatency, 0x4000b);
-	pwtest_int_eq(_SPA_TYPE_OBJECT_LAST, 0x4000c);
+	pwtest_int_eq(SPA_TYPE_OBJECT_ParamProcessLatency, 0x4000c);
+	pwtest_int_eq(_SPA_TYPE_OBJECT_LAST, 0x4000d);
 
 	pwtest_int_eq(SPA_TYPE_VENDOR_PipeWire, 0x02000000);
 	pwtest_int_eq(SPA_TYPE_VENDOR_Other, 0x7f000000);

417c9ab887f50cb692eb485b7293499f38f71c5d
Wim Taymans <wtaymans@redhat.com>
2021-06-25 16:44:26 +0200

    modules: add an example sink and source

---
+++ src/modules/module-example-source.c
@@ -0,0 +1,485 @@
+/* PipeWire
+ *
+ * Copyright © 2021 Wim Taymans
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <limits.h>
+#include <math.h>
+
+#include "config.h"
+
+#include <spa/utils/result.h>
+#include <spa/utils/string.h>
+#include <spa/utils/json.h>
+#include <spa/utils/ringbuffer.h>
+#include <spa/debug/pod.h>
+#include <spa/pod/builder.h>
+#include <spa/param/audio/format-utils.h>
+#include <spa/param/audio/raw.h>
+
+#include <pipewire/impl.h>
+#include <pipewire/i18n.h>
+
+/** \page page_module_example_source PipeWire Module: Example Source
+ */
+
+#define NAME "example-source"
+
+#define DEFAULT_FORMAT "S16"
+#define DEFAULT_RATE "48000"
+#define DEFAULT_CHANNELS "2"
+#define DEFAULT_POSITION "[ FL FR ]"
+
+#define MODULE_USAGE	"[ node.latency=<latency as fraction> ] "				\
+			"[ node.name=<name of the nodes> ] "					\
+			"[ node.description=<description of the nodes> ] "			\
+			"[ audio.format=<format, default:"DEFAULT_FORMAT"> ] "			\
+			"[ audio.rate=<sample rate, default:"DEFAULT_RATE"> ] "			\
+			"[ audio.channels=<number of channels, default:"DEFAULT_CHANNELS"> ] "	\
+			"[ audio.position=<channel map, default:"DEFAULT_POSITION"> ] "		\
+			"[ stream.props=<properties> ] "
+
+
+static const struct spa_dict_item module_props[] = {
+	{ PW_KEY_MODULE_AUTHOR, "Wim Taymans <wim.taymans@gmail.com>" },
+	{ PW_KEY_MODULE_DESCRIPTION, "An example audio source" },
+	{ PW_KEY_MODULE_USAGE, MODULE_USAGE },
+	{ PW_KEY_MODULE_VERSION, PACKAGE_VERSION },
+};
+
+struct impl {
+	struct pw_context *context;
+
+	struct pw_properties *props;
+
+	struct pw_impl_module *module;
+	struct pw_work_queue *work;
+
+	struct spa_hook module_listener;
+
+	struct pw_core *core;
+	struct spa_hook core_proxy_listener;
+	struct spa_hook core_listener;
+
+	struct pw_properties *stream_props;
+	struct pw_stream *stream;
+	struct spa_hook stream_listener;
+	struct spa_io_rate_match *rate_match;
+	struct spa_audio_info_raw info;
+	uint32_t frame_size;
+
+	unsigned int do_disconnect:1;
+	unsigned int unloading:1;
+};
+
+static void do_unload_module(void *obj, void *data, int res, uint32_t id)
+{
+	struct impl *impl = data;
+	pw_impl_module_destroy(impl->module);
+}
+
+static void unload_module(struct impl *impl)
+{
+	if (!impl->unloading) {
+		impl->unloading = true;
+		pw_work_queue_add(impl->work, impl, 0, do_unload_module, impl);
+	}
+}
+
+static void stream_destroy(void *d)
+{
+	struct impl *impl = d;
+	spa_hook_remove(&impl->stream_listener);
+	impl->stream = NULL;
+}
+
+static void stream_state_changed(void *d, enum pw_stream_state old,
+		enum pw_stream_state state, const char *error)
+{
+	struct impl *impl = d;
+	switch (state) {
+	case PW_STREAM_STATE_ERROR:
+	case PW_STREAM_STATE_UNCONNECTED:
+		unload_module(impl);
+		break;
+	case PW_STREAM_STATE_PAUSED:
+	case PW_STREAM_STATE_STREAMING:
+		break;
+	default:
+		break;
+	}
+}
+
+static void capture_stream_process(void *d)
+{
+	struct impl *impl = d;
+	struct pw_buffer *buf;
+	struct spa_data *bd;
+	void *data;
+	uint32_t size;
+
+	if ((buf = pw_stream_dequeue_buffer(impl->stream)) == NULL) {
+		pw_log_warn("out of buffers: %m");
+		return;
+	}
+
+	bd = &buf->buffer->datas[0];
+
+	data = bd->data;
+	if (impl->rate_match)
+		size = SPA_MIN(impl->rate_match->size * impl->frame_size, bd->maxsize);
+	else
+		size = bd->maxsize;
+
+	/* fill buffer contents here */
+	pw_log_info("fill buffer data %p with up to %u bytes", data, size);
+
+	bd->chunk->size = size;
+	bd->chunk->stride = impl->frame_size;
+	bd->chunk->offset = 0;
+
+	pw_stream_queue_buffer(impl->stream, buf);
+}
+
+static void stream_io_changed(void *data, uint32_t id, void *area, uint32_t size)
+{
+	struct impl *impl = data;
+	switch (id) {
+	case SPA_IO_RateMatch:
+		impl->rate_match = area;
+		break;
+	}
+}
+
+static const struct pw_stream_events capture_stream_events = {
+	PW_VERSION_STREAM_EVENTS,
+	.destroy = stream_destroy,
+	.state_changed = stream_state_changed,
+	.io_changed = stream_io_changed,
+	.process = capture_stream_process
+};
+
+static int create_stream(struct impl *impl)
+{
+	int res;
+	uint32_t n_params;
+	const struct spa_pod *params[1];
+	uint8_t buffer[1024];
+	struct spa_pod_builder b;
+
+	impl->stream = pw_stream_new(impl->core, "example source", impl->stream_props);
+	impl->stream_props = NULL;
+
+	if (impl->stream == NULL)
+		return -errno;
+
+	pw_stream_add_listener(impl->stream,
+			&impl->stream_listener,
+			&capture_stream_events, impl);
+
+	n_params = 0;
+	spa_pod_builder_init(&b, buffer, sizeof(buffer));
+	params[n_params++] = spa_format_audio_raw_build(&b,
+			SPA_PARAM_EnumFormat, &impl->info);
+
+	if ((res = pw_stream_connect(impl->stream,
+			PW_DIRECTION_OUTPUT,
+			PW_ID_ANY,
+			PW_STREAM_FLAG_AUTOCONNECT |
+			PW_STREAM_FLAG_MAP_BUFFERS |
+			PW_STREAM_FLAG_RT_PROCESS,
+			params, n_params)) < 0)
+		return res;
+
+	return 0;
+}
+
+static void core_error(void *data, uint32_t id, int seq, int res, const char *message)
+{
+	struct impl *impl = data;
+
+	pw_log_error("error id:%u seq:%d res:%d (%s): %s",
+			id, seq, res, spa_strerror(res), message);
+
+	if (id == PW_ID_CORE && res == -EPIPE)
+		unload_module(impl);
+}
+
+static const struct pw_core_events core_events = {
+	PW_VERSION_CORE_EVENTS,
+	.error = core_error,
+};
+
+static void core_destroy(void *d)
+{
+	struct impl *impl = d;
+	spa_hook_remove(&impl->core_listener);
+	impl->core = NULL;
+	unload_module(impl);
+}
+
+static const struct pw_proxy_events core_proxy_events = {
+	.destroy = core_destroy,
+};
+
+static void impl_destroy(struct impl *impl)
+{
+	if (impl->stream)
+		pw_stream_destroy(impl->stream);
+	if (impl->core && impl->do_disconnect)
+		pw_core_disconnect(impl->core);
+
+	pw_properties_free(impl->stream_props);
+	pw_properties_free(impl->props);
+
+	if (impl->work)
+		pw_work_queue_cancel(impl->work, impl, SPA_ID_INVALID);
+	free(impl);
+}
+
+static void module_destroy(void *data)
+{
+	struct impl *impl = data;
+	impl->unloading = true;
+	spa_hook_remove(&impl->module_listener);
+	impl_destroy(impl);
+}
+
+static const struct pw_impl_module_events module_events = {
+	PW_VERSION_IMPL_MODULE_EVENTS,
+	.destroy = module_destroy,
+};
+
+static inline uint32_t format_from_name(const char *name, size_t len)
+{
+	int i;
+	for (i = 0; spa_type_audio_format[i].name; i++) {
+		if (strncmp(name, spa_debug_type_short_name(spa_type_audio_format[i].name), len) == 0)
+			return spa_type_audio_format[i].type;
+	}
+	return SPA_AUDIO_FORMAT_UNKNOWN;
+}
+
+static uint32_t channel_from_name(const char *name)
+{
+	int i;
+	for (i = 0; spa_type_audio_channel[i].name; i++) {
+		if (spa_streq(name, spa_debug_type_short_name(spa_type_audio_channel[i].name)))
+			return spa_type_audio_channel[i].type;
+	}
+	return SPA_AUDIO_CHANNEL_UNKNOWN;
+}
+
+static void parse_position(struct spa_audio_info_raw *info, const char *val, size_t len)
+{
+	struct spa_json it[2];
+	char v[256];
+
+	spa_json_init(&it[0], val, len);
+        if (spa_json_enter_array(&it[0], &it[1]) <= 0)
+                spa_json_init(&it[1], val, len);
+
+	info->channels = 0;
+	while (spa_json_get_string(&it[1], v, sizeof(v)) > 0 &&
+	    info->channels < SPA_AUDIO_MAX_CHANNELS) {
+		info->position[info->channels++] = channel_from_name(v);
+	}
+}
+
+static int parse_audio_info(struct impl *impl)
+{
+	struct pw_properties *props = impl->stream_props;
+	struct spa_audio_info_raw *info = &impl->info;
+	const char *str;
+
+	spa_zero(*info);
+
+	if ((str = pw_properties_get(props, PW_KEY_AUDIO_FORMAT)) == NULL)
+		str = DEFAULT_FORMAT;
+	info->format = format_from_name(str, strlen(str));
+	switch (info->format) {
+	case SPA_AUDIO_FORMAT_S8:
+	case SPA_AUDIO_FORMAT_U8:
+		impl->frame_size = 1;
+		break;
+	case SPA_AUDIO_FORMAT_S16:
+		impl->frame_size = 2;
+		break;
+	case SPA_AUDIO_FORMAT_S24:
+		impl->frame_size = 3;
+		break;
+	case SPA_AUDIO_FORMAT_S24_32:
+	case SPA_AUDIO_FORMAT_S32:
+	case SPA_AUDIO_FORMAT_F32:
+		impl->frame_size = 4;
+		break;
+	case SPA_AUDIO_FORMAT_F64:
+		impl->frame_size = 8;
+		break;
+	default:
+		pw_log_error("unsupported format '%s'", str);
+		return -EINVAL;
+	}
+	if ((str = pw_properties_get(props, PW_KEY_AUDIO_RATE)) == NULL)
+		str = DEFAULT_RATE;
+	info->rate = atoi(str);
+	if (info->rate == 0) {
+		pw_log_error("invalid rate '%s'", str);
+		return -EINVAL;
+	}
+	if ((str = pw_properties_get(props, PW_KEY_AUDIO_CHANNELS)) == NULL)
+		str = DEFAULT_CHANNELS;
+	info->channels = atoi(str);
+	if ((str = pw_properties_get(props, SPA_KEY_AUDIO_POSITION)) == NULL)
+		str = DEFAULT_POSITION;
+	parse_position(info, str, strlen(str));
+	if (info->channels == 0) {
+		pw_log_error("invalid channels '%s'", str);
+		return -EINVAL;
+	}
+	impl->frame_size *= info->channels;
+
+	return 0;
+}
+
+static void copy_props(struct impl *impl, struct pw_properties *props, const char *key)
+{
+	const char *str;
+	if ((str = pw_properties_get(props, key)) != NULL) {
+		if (pw_properties_get(impl->stream_props, key) == NULL)
+			pw_properties_set(impl->stream_props, key, str);
+	}
+}
+
+SPA_EXPORT
+int pipewire__module_init(struct pw_impl_module *module, const char *args)
+{
+	struct pw_context *context = pw_impl_module_get_context(module);
+	struct pw_properties *props = NULL;
+	struct impl *impl;
+	const char *str;
+	int res;
+
+	impl = calloc(1, sizeof(struct impl));
+	if (impl == NULL)
+		return -errno;
+
+	pw_log_debug("module %p: new %s", impl, args);
+
+	if (args == NULL)
+		args = "";
+
+	props = pw_properties_new_string(args);
+	if (props == NULL) {
+		res = -errno;
+		pw_log_error( "can't create properties: %m");
+		goto error;
+	}
+	impl->props = props;
+
+	impl->stream_props = pw_properties_new(NULL, NULL);
+	if (impl->stream_props == NULL) {
+		res = -errno;
+		pw_log_error( "can't create properties: %m");
+		goto error;
+	}
+
+	impl->module = module;
+	impl->context = context;
+	impl->work = pw_context_get_work_queue(context);
+	if (impl->work == NULL) {
+		res = -errno;
+		pw_log_error( "can't get work queue: %m");
+		goto error;
+	}
+
+	if (pw_properties_get(props, PW_KEY_NODE_GROUP) == NULL)
+		pw_properties_set(props, PW_KEY_NODE_GROUP, "pipewire.dummy");
+	if (pw_properties_get(props, PW_KEY_NODE_VIRTUAL) == NULL)
+		pw_properties_set(props, PW_KEY_NODE_VIRTUAL, "true");
+
+	if (pw_properties_get(props, PW_KEY_MEDIA_CLASS) == NULL)
+		pw_properties_set(props, PW_KEY_MEDIA_CLASS, "Audio/Source");
+
+	if ((str = pw_properties_get(props, "stream.props")) != NULL)
+		pw_properties_update_string(impl->stream_props, str, strlen(str));
+
+	copy_props(impl, props, PW_KEY_AUDIO_RATE);
+	copy_props(impl, props, PW_KEY_AUDIO_CHANNELS);
+	copy_props(impl, props, SPA_KEY_AUDIO_POSITION);
+	copy_props(impl, props, PW_KEY_NODE_NAME);
+	copy_props(impl, props, PW_KEY_NODE_DESCRIPTION);
+	copy_props(impl, props, PW_KEY_NODE_GROUP);
+	copy_props(impl, props, PW_KEY_NODE_LATENCY);
+	copy_props(impl, props, PW_KEY_NODE_VIRTUAL);
+	copy_props(impl, props, PW_KEY_MEDIA_CLASS);
+
+	if ((res = parse_audio_info(impl)) < 0) {
+		pw_log_error( "can't parse audio format");
+		goto error;
+	}
+
+	impl->core = pw_context_get_object(impl->context, PW_TYPE_INTERFACE_Core);
+	if (impl->core == NULL) {
+		str = pw_properties_get(props, PW_KEY_REMOTE_NAME);
+		impl->core = pw_context_connect(impl->context,
+				pw_properties_new(
+					PW_KEY_REMOTE_NAME, str,
+					NULL),
+				0);
+		impl->do_disconnect = true;
+	}
+	if (impl->core == NULL) {
+		res = -errno;
+		pw_log_error("can't connect: %m");
+		goto error;
+	}
+
+	pw_proxy_add_listener((struct pw_proxy*)impl->core,
+			&impl->core_proxy_listener,
+			&core_proxy_events, impl);
+	pw_core_add_listener(impl->core,
+			&impl->core_listener,
+			&core_events, impl);
+
+	if ((res = create_stream(impl)) < 0)
+		goto error;
+
+	pw_impl_module_add_listener(module, &impl->module_listener, &module_events, impl);
+
+	pw_impl_module_update_properties(module, &SPA_DICT_INIT_ARRAY(module_props));
+
+	return 0;
+
+error:
+	impl_destroy(impl);
+	return res;
+}

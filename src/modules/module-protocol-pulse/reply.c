c68a7cd6035c097d1ff0ccec38635022b8e0af6d
Wim Taymans <wtaymans@redhat.com>
2021-06-28 12:43:31 +0200

    pulse-server: silence the ENOTSUP warnings
    To stop the device restore extension warnings.

--- src/modules/module-protocol-pulse/reply.c
+++ src/modules/module-protocol-pulse/reply.c
@@ -52,14 +52,24 @@ int reply_error(struct client *client, uint32_t command, uint32_t tag, int res)
 	struct message *reply;
 	uint32_t error = res_to_err(res);
 	const char *name;
+	enum spa_log_level level;
 
 	if (command < COMMAND_MAX)
 		name = commands[command].name;
 	else
 		name = "invalid";
 
-	pw_log(res == -ENOENT ? SPA_LOG_LEVEL_INFO : SPA_LOG_LEVEL_WARN,
-	       "client %p [%s]: ERROR command:%d (%s) tag:%u error:%u (%s)",
+	switch (res) {
+	case -ENOENT:
+	case -ENOTSUP:
+		level = SPA_LOG_LEVEL_INFO;
+		break;
+	default:
+		level = SPA_LOG_LEVEL_WARN;
+		break;
+	}
+
+	pw_log(level, "client %p [%s]: ERROR command:%d (%s) tag:%u error:%u (%s)",
 	       client, client->name, command, name, tag, error, spa_strerror(res));
 
 	reply = message_alloc(impl, -1, 0);

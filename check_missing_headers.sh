45c045382e76755f9209781aef93303310a60a12
George Kiagiadakis <george.kiagiadakis@collabora.com>
2021-07-01 12:41:36 +0300

    check_missing_headers.sh: stop looking in the old src/extensions dir

--- check_missing_headers.sh
+++ check_missing_headers.sh
@@ -10,11 +10,6 @@ do
 	[ -f "$PREFIX/include/spa-0.2/$i" ] || LIST="$i $LIST"
 done
 
-for i in $(find src/extensions -name '*.h' | sed s#src/#pipewire/#);
-do
-	[ -f "$PREFIX/include/pipewire-0.3/$i" ] || LIST="$i $LIST"
-done
-
 for i in $(find src/pipewire -name '*.h' -a -not -name '*private.h' | sed s#src/##);
 do
 	[ -f "$PREFIX/include/pipewire-0.3/$i" ] || LIST="$i $LIST"

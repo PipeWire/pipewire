43e4625bbe4a1ad5fca4cf7d82ccacc9a2dea38e
Gleb Popov <6yearold@gmail.com>
2021-07-01 15:48:01 +0300

    Use real pthread_setname_np implementation on old FreeBSD.
    pthread_setname_np differs from pthread_set_name_np in the return value, so
    it is not correct to just #define one to another.

--- src/pipewire/thread-loop.c
+++ src/pipewire/thread-loop.c
@@ -37,8 +37,14 @@
 #define pw_thread_loop_events_destroy(o)	pw_thread_loop_events_emit(o, destroy, 0)
 
 #ifdef __FreeBSD__
-#include <pthread_np.h>
-#define pthread_setname_np pthread_set_name_np
+#include <sys/param.h>
+#if __FreeBSD_version < 1202000
+int pthread_setname_np(pthread_t thread, const char *name)
+{
+	pthread_set_name_np(thread, name);
+	return 0;
+}
+#endif
 #endif
 
 /** \cond */

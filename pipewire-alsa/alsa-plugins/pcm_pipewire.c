7846687df869d99d95701b419080234450fd6d8d
Wim Taymans <wtaymans@redhat.com>
2021-08-18 09:56:20 +0200

    alsa: suggest rate to graph with node.rate

--- pipewire-alsa/alsa-plugins/pcm_pipewire.c
+++ pipewire-alsa/alsa-plugins/pcm_pipewire.c
@@ -500,6 +500,8 @@ static int snd_pcm_pipewire_prepare(snd_pcm_ioplug_t *io)
 
 	if (pw_properties_get(props, PW_KEY_NODE_LATENCY) == NULL)
 		pw_properties_setf(props, PW_KEY_NODE_LATENCY, "%lu/%u", pw->min_avail, io->rate);
+	if (pw_properties_get(props, PW_KEY_NODE_RATE) == NULL)
+		pw_properties_setf(props, PW_KEY_NODE_RATE, "1/%u", io->rate);
 	if (pw->target != NULL &&
 		pw_properties_get(props, PW_KEY_NODE_TARGET) == NULL)
 		pw_properties_setf(props, PW_KEY_NODE_TARGET, "%s", pw->target);

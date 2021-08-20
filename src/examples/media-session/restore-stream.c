24f61deefdbbf1aabcef6009a636fc6f64623e19
Wim Taymans <wtaymans@redhat.com>
2021-08-20 13:44:04 +0200

    media-session: also check restore-stream when info updates
    We only need an info update to restore the stream so check for that
    in addition to a param update.
    
    The info update can be used to restore the stream and the param update
    to update the stored value.

--- src/examples/media-session/restore-stream.c
+++ src/examples/media-session/restore-stream.c
@@ -457,7 +457,7 @@ static void object_update(void *data)
 	pw_log_info(NAME" %p: stream %p %08x/%08x", impl, str,
 			str->obj->obj.changed, str->obj->obj.avail);
 
-	if (str->obj->obj.changed & SM_NODE_CHANGE_MASK_PARAMS)
+	if (str->obj->obj.changed & (SM_NODE_CHANGE_MASK_INFO | SM_NODE_CHANGE_MASK_PARAMS))
 		update_stream(str);
 }
 

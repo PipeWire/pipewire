ae9f90ecccce4fdbca267385816d3b1266e91104
Wim Taymans <wtaymans@redhat.com>
2021-09-17 18:06:13 +0200

    dsdplay: implement data processing
    Simplify channel layout management a little.
    Use the bitorder and interleaving to read and format the DSD in the
    buffer.

--- src/tools/pw-dsdplay.c
+++ src/tools/pw-dsdplay.c
@@ -33,6 +33,7 @@
 #include <getopt.h>
 
 #include <spa/param/audio/format-utils.h>
+#include <spa/param/audio/layout.h>
 
 #include <pipewire/pipewire.h>
 
@@ -208,6 +209,55 @@ exit_free:
         return NULL;
 }
 
+static const uint8_t bitrev[256] = {
+	0x00, 0x80, 0x40, 0xc0, 0x20, 0xa0, 0x60, 0xe0, 0x10, 0x90, 0x50, 0xd0, 0x30, 0xb0, 0x70, 0xf0,
+	0x08, 0x88, 0x48, 0xc8, 0x28, 0xa8, 0x68, 0xe8, 0x18, 0x98, 0x58, 0xd8, 0x38, 0xb8, 0x78, 0xf8,
+	0x04, 0x84, 0x44, 0xc4, 0x24, 0xa4, 0x64, 0xe4, 0x14, 0x94, 0x54, 0xd4, 0x34, 0xb4, 0x74, 0xf4,
+	0x0c, 0x8c, 0x4c, 0xcc, 0x2c, 0xac, 0x6c, 0xec, 0x1c, 0x9c, 0x5c, 0xdc, 0x3c, 0xbc, 0x7c, 0xfc,
+	0x02, 0x82, 0x42, 0xc2, 0x22, 0xa2, 0x62, 0xe2, 0x12, 0x92, 0x52, 0xd2, 0x32, 0xb2, 0x72, 0xf2,
+	0x0a, 0x8a, 0x4a, 0xca, 0x2a, 0xaa, 0x6a, 0xea, 0x1a, 0x9a, 0x5a, 0xda, 0x3a, 0xba, 0x7a, 0xfa,
+	0x06, 0x86, 0x46, 0xc6, 0x26, 0xa6, 0x66, 0xe6, 0x16, 0x96, 0x56, 0xd6, 0x36, 0xb6, 0x76, 0xf6,
+	0x0e, 0x8e, 0x4e, 0xce, 0x2e, 0xae, 0x6e, 0xee, 0x1e, 0x9e, 0x5e, 0xde, 0x3e, 0xbe, 0x7e, 0xfe,
+	0x01, 0x81, 0x41, 0xc1, 0x21, 0xa1, 0x61, 0xe1, 0x11, 0x91, 0x51, 0xd1, 0x31, 0xb1, 0x71, 0xf1,
+	0x09, 0x89, 0x49, 0xc9, 0x29, 0xa9, 0x69, 0xe9, 0x19, 0x99, 0x59, 0xd9, 0x39, 0xb9, 0x79, 0xf9,
+	0x05, 0x85, 0x45, 0xc5, 0x25, 0xa5, 0x65, 0xe5, 0x15, 0x95, 0x55, 0xd5, 0x35, 0xb5, 0x75, 0xf5,
+	0x0d, 0x8d, 0x4d, 0xcd, 0x2d, 0xad, 0x6d, 0xed, 0x1d, 0x9d, 0x5d, 0xdd, 0x3d, 0xbd, 0x7d, 0xfd,
+	0x03, 0x83, 0x43, 0xc3, 0x23, 0xa3, 0x63, 0xe3, 0x13, 0x93, 0x53, 0xd3, 0x33, 0xb3, 0x73, 0xf3,
+	0x0b, 0x8b, 0x4b, 0xcb, 0x2b, 0xab, 0x6b, 0xeb, 0x1b, 0x9b, 0x5b, 0xdb, 0x3b, 0xbb, 0x7b, 0xfb,
+	0x07, 0x87, 0x47, 0xc7, 0x27, 0xa7, 0x67, 0xe7, 0x17, 0x97, 0x57, 0xd7, 0x37, 0xb7, 0x77, 0xf7,
+	0x0f, 0x8f, 0x4f, 0xcf, 0x2f, 0xaf, 0x6f, 0xef, 0x1f, 0x9f, 0x5f, 0xdf, 0x3f, 0xbf, 0x7f, 0xff,
+};
+
+ssize_t
+dsf_file_read(struct dsf_file *f, void *data, size_t samples, uint32_t interleave, bool lsb)
+{
+	uint8_t *d = data;
+	const uint8_t *s;
+	uint32_t i, j, k, total = 0, size, stride, bytes;
+	bool rev = lsb != f->info.lsb;
+
+	stride = f->info.channels * interleave;
+	size = f->info.channels * f->info.blocksize;
+
+	bytes = samples * stride;
+	bytes -= bytes % f->info.blocksize;
+
+	s = f->p;
+	while (total < bytes) {
+		for (i = 0; i < f->info.blocksize; i += interleave)  {
+			for (j = 0; j < f->info.channels; j++) {
+				const uint8_t *c = &s[f->info.blocksize * j + i];
+				for (k = 0; k < interleave; k++)
+					*d++ = rev ? bitrev[c[k]] : c[k];
+			}
+		}
+		s += size;
+		total += size;
+	}
+	f->p += total;
+	return total / stride;
+}
+
 int dsf_file_close(struct dsf_file *f)
 {
 	if (f->mode == 1) {
@@ -229,6 +279,8 @@ struct data {
 
 	struct dsf_file *f;
 	struct dsf_file_info info;
+
+	struct spa_audio_info_dsd format;
 };
 
 static void on_process(void *userdata)
@@ -236,8 +288,9 @@ static void on_process(void *userdata)
 	struct data *data = userdata;
 	struct pw_buffer *b;
 	struct spa_buffer *buf;
-	int n_frames, stride;
-	uint8_t *p;
+	ssize_t stride;
+	uint32_t samples;
+	uint8_t *d;
 
 	if ((b = pw_stream_dequeue_buffer(data->stream)) == NULL) {
 		pw_log_warn("out of buffers: %m");
@@ -245,22 +298,52 @@ static void on_process(void *userdata)
 	}
 
 	buf = b->buffer;
-	if ((p = buf->datas[0].data) == NULL)
+	if ((d = buf->datas[0].data) == NULL)
 		return;
 
-	stride = data->info.channels * data->info.blocksize;
+	stride = data->info.channels * data->format.interleave;
 
-	n_frames = buf->datas[0].maxsize / stride;
+	samples = dsf_file_read(data->f, d, buf->datas[0].maxsize / stride,
+			data->format.interleave,
+			data->format.bitorder == SPA_PARAM_BITORDER_lsb);
 
 	buf->datas[0].chunk->offset = 0;
 	buf->datas[0].chunk->stride = stride;
-	buf->datas[0].chunk->size = n_frames * stride;
+	buf->datas[0].chunk->size = samples * stride;
 
 	pw_stream_queue_buffer(data->stream, b);
 }
 
+static void
+on_param_changed(void *userdata, uint32_t id, const struct spa_pod *param)
+{
+	struct data *data = userdata;
+	struct spa_audio_info info = { 0 };
+	int err;
+
+	if (id != SPA_PARAM_Format || param == NULL)
+		return;
+
+	if ((err = spa_format_parse(param, &info.media_type, &info.media_subtype)) < 0)
+		return;
+
+	if (info.media_type != SPA_MEDIA_TYPE_audio ||
+	    info.media_subtype != SPA_MEDIA_SUBTYPE_dsd)
+		return;
+
+	if (spa_format_audio_dsd_parse(param, &info.info.dsd) < 0)
+		return;
+
+	data->format = info.info.dsd;
+	fprintf(stderr, "output:\n");
+	fprintf(stderr, " bitorder: %s\n",
+			data->format.bitorder == SPA_PARAM_BITORDER_lsb ? "lsb" : "msb");
+	fprintf(stderr, " interleave: %d\n", data->format.interleave);
+}
+
 static const struct pw_stream_events stream_events = {
 	PW_VERSION_STREAM_EVENTS,
+	.param_changed = on_param_changed,
 	.process = on_process,
 };
 
@@ -270,12 +353,28 @@ static void do_quit(void *userdata, int signal_number)
 	pw_main_loop_quit(data->loop);
 }
 
+struct layout_info {
+	 uint32_t type;
+	 struct spa_audio_layout_info info;
+};
+
+static const struct layout_info layouts[] = {
+	{ 1, { SPA_AUDIO_LAYOUT_Mono, }, },
+	{ 2, { SPA_AUDIO_LAYOUT_Stereo, }, },
+	{ 3, { SPA_AUDIO_LAYOUT_2FC }, },
+	{ 4, { SPA_AUDIO_LAYOUT_Quad }, },
+	{ 5, { SPA_AUDIO_LAYOUT_3_1 }, },
+	{ 6, { SPA_AUDIO_LAYOUT_5_0R }, },
+	{ 7, { SPA_AUDIO_LAYOUT_5_1R }, },
+};
+
 int handle_dsd_playback(struct data *data)
 {
 	const struct spa_pod *params[1];
 	uint8_t buffer[1024];
 	struct spa_pod_builder b = SPA_POD_BUILDER_INIT(buffer, sizeof(buffer));
 	struct spa_audio_info_dsd info;
+	size_t i;
 
 	data->loop = pw_main_loop_new(NULL);
 
@@ -295,50 +394,15 @@ int handle_dsd_playback(struct data *data)
 			data);
 
 	spa_zero(info);
-	info.bitorder = data->info.lsb ?  SPA_PARAM_BITORDER_lsb : SPA_PARAM_BITORDER_msb;
 	info.channels = data->info.channels;
-	info.rate = data->info.rate;
+	info.rate = data->info.rate / 8;
 
-	switch (data->info.channel_type) {
-	case 1:
-		info.position[0] = SPA_AUDIO_CHANNEL_MONO;
-		break;
-	case 3:
-		info.position[2] = SPA_AUDIO_CHANNEL_FC;
-		SPA_FALLTHROUGH;
-	case 2:
-		info.position[0] = SPA_AUDIO_CHANNEL_FL;
-		info.position[1] = SPA_AUDIO_CHANNEL_FR;
-		break;
-	case 4:
-		info.position[0] = SPA_AUDIO_CHANNEL_FL;
-		info.position[1] = SPA_AUDIO_CHANNEL_FR;
-		info.position[2] = SPA_AUDIO_CHANNEL_RL;
-		info.position[3] = SPA_AUDIO_CHANNEL_RR;
-		break;
-	case 5:
-		info.position[0] = SPA_AUDIO_CHANNEL_FL;
-		info.position[1] = SPA_AUDIO_CHANNEL_FR;
-		info.position[2] = SPA_AUDIO_CHANNEL_FC;
-		info.position[3] = SPA_AUDIO_CHANNEL_LFE;
-		break;
-	case 6:
-		info.position[0] = SPA_AUDIO_CHANNEL_FL;
-		info.position[1] = SPA_AUDIO_CHANNEL_FR;
-		info.position[2] = SPA_AUDIO_CHANNEL_FC;
-		info.position[3] = SPA_AUDIO_CHANNEL_RL;
-		info.position[4] = SPA_AUDIO_CHANNEL_RR;
-		break;
-	case 7:
-		info.position[0] = SPA_AUDIO_CHANNEL_FL;
-		info.position[1] = SPA_AUDIO_CHANNEL_FR;
-		info.position[2] = SPA_AUDIO_CHANNEL_FC;
-		info.position[3] = SPA_AUDIO_CHANNEL_LFE;
-		info.position[4] = SPA_AUDIO_CHANNEL_RL;
-		info.position[5] = SPA_AUDIO_CHANNEL_RR;
-		break;
-	default:
-		break;
+	for (i = 0; i < SPA_N_ELEMENTS(layouts); i++) {
+		if (layouts[i].type != data->info.channel_type)
+			continue;
+		info.channels = layouts[i].info.n_channels;
+		memcpy(info.position, layouts[i].info.position,
+				info.channels * sizeof(uint32_t));
 	}
 
 	params[0] = spa_format_audio_dsd_build(&b, SPA_PARAM_EnumFormat, &info);
@@ -349,8 +413,7 @@ int handle_dsd_playback(struct data *data)
 			  PW_DIRECTION_OUTPUT,
 			  PW_ID_ANY,
 			  PW_STREAM_FLAG_AUTOCONNECT |
-			  PW_STREAM_FLAG_MAP_BUFFERS |
-			  PW_STREAM_FLAG_RT_PROCESS,
+			  PW_STREAM_FLAG_MAP_BUFFERS,
 			  params, 1);
 
 	/* and wait while we let things run */

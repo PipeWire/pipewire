e94adaac2408bf0351b9b6b34eb24f1ac75d3d13
Gleb Popov <6yearold@gmail.com>
2021-10-22 17:50:42 +0300

    Don't include byteswap.h on FreeBSD, use sys/endian.h instead

--- spa/plugins/audioconvert/fmt-ops.h
+++ spa/plugins/audioconvert/fmt-ops.h
@@ -23,7 +23,13 @@
  */
 
 #include <math.h>
+#ifdef __FreeBSD__
+#include <sys/endian.h>
+#define bswap_16 bswap16
+#define bswap_32 bswap32
+#else
 #include <byteswap.h>
+#endif
 
 #include <spa/utils/defs.h>
 

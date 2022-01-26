6c5c3dc09013ef4569ec074fa15b0685edea4408
Gleb Popov <6yearold@gmail.com>
2022-01-26 14:32:47 +0300

    Fix build on recent FreeBSD 13-STABLE

--- spa/tests/stress-ringbuffer.c
+++ spa/tests/stress-ringbuffer.c
@@ -13,7 +13,8 @@
 
 #ifdef __FreeBSD__
 #include <sys/param.h>
-#if __FreeBSD_version < 1400043
+#if (__FreeBSD_version >= 1400000 && __FreeBSD_version < 1400043) \
+    || (__FreeBSD_version < 1300523)
 static int sched_getcpu(void) { return -1; };
 #endif
 #endif

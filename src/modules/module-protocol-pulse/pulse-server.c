5a55e1a47a9ce3882e057aa9c59b7c7a4c8af357
Wim Taymans <wtaymans@redhat.com>
2022-03-02 17:53:08 +0100

    pulse-server: handle pulse.properties
    Load properties from pulse.properties section so that they can have
    overrides.

--- src/modules/module-protocol-pulse/pulse-server.c
+++ src/modules/module-protocol-pulse/pulse-server.c
@@ -5350,6 +5350,8 @@ struct pw_protocol_pulse *pw_protocol_pulse_new(struct pw_context *context,
 	support = pw_context_get_support(context, &n_support);
 	cpu = spa_support_find(support, n_support, SPA_TYPE_INTERFACE_CPU);
 
+	pw_context_conf_update_props(context, "pulse.properties", props);
+
 	if ((str = pw_properties_get(props, "vm.overrides")) != NULL) {
 		if (cpu != NULL && spa_cpu_get_vm_type(cpu) != SPA_CPU_VM_NONE)
 			pw_properties_update_string(props, str, strlen(str));

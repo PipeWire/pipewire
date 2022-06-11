354a04c91dbc60fcd16082baef66889827e1f8f3
Ole Salscheider <ole@salscheider.org>
2022-06-11 23:53:59 +0200

    Channel mixer: Remove channelmix_f32_2_4_sse
    It does not have PSD upmixing implemented and does not allow to disable
    the simple upmixing algorithm either.
    
    Fixes #2438.

--- spa/plugins/audioconvert/channelmix-ops.c
+++ spa/plugins/audioconvert/channelmix-ops.c
@@ -79,9 +79,6 @@ static const struct channelmix_info {
 	{ 2, MASK_STEREO, 1, MASK_MONO, channelmix_f32_2_1_c, 0, "f32_2_1_c" },
 	{ 4, MASK_QUAD, 1, MASK_MONO, channelmix_f32_4_1_c, 0, "f32_4_1_c" },
 	{ 4, MASK_3_1, 1, MASK_MONO, channelmix_f32_4_1_c, 0, "f32_4_1_c" },
-#if defined (HAVE_SSE)
-	{ 2, MASK_STEREO, 4, MASK_QUAD, channelmix_f32_2_4_sse, SPA_CPU_FLAG_SSE, "f32_2_4_sse" },
-#endif
 	{ 2, MASK_STEREO, 4, MASK_QUAD, channelmix_f32_2_4_c, 0, "f32_2_4_c" },
 	{ 2, MASK_STEREO, 4, MASK_3_1, channelmix_f32_2_3p1_c, 0, "f32_2_3p1_c" },
 	{ 2, MASK_STEREO, 6, MASK_5_1, channelmix_f32_2_5p1_c, 0, "f32_2_5p1_c" },

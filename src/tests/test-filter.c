e92e90bd3f8467800e0a8d0dbfe0b5df40989ffe
Wim Taymans <wtaymans@redhat.com>
2022-07-08 15:16:41 +0200

    tests: add filter unit test

--- src/tests/test-filter.c
+++ src/tests/test-filter.c
@@ -1,6 +1,6 @@
 /* PipeWire
  *
- * Copyright © 2019 Wim Taymans
+ * Copyright © 2022 Wim Taymans
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -24,7 +24,7 @@
 
 #include <pipewire/pipewire.h>
 #include <pipewire/main-loop.h>
-#include <pipewire/stream.h>
+#include <pipewire/filter.h>
 
 #include <spa/utils/string.h>
 
@@ -39,24 +39,21 @@ static void test_abi(void)
 	static const struct {
 		uint32_t version;
 		void (*destroy) (void *data);
-		void (*state_changed) (void *data, enum pw_stream_state old,
-			enum pw_stream_state state, const char *error);
-		void (*control_info) (void *data, uint32_t id, const struct pw_stream_control *control);
-		void (*io_changed) (void *data, uint32_t id, void *area, uint32_t size);
-		void (*param_changed) (void *data, uint32_t id, const struct spa_pod *param);
-		void (*add_buffer) (void *data, struct pw_buffer *buffer);
-		void (*remove_buffer) (void *data, struct pw_buffer *buffer);
-		void (*process) (void *data);
+		void (*state_changed) (void *data, enum pw_filter_state old,
+			enum pw_filter_state state, const char *error);
+		void (*io_changed) (void *data, void *port_data, uint32_t id, void *area, uint32_t size);
+		void (*param_changed) (void *data, void *port_data, uint32_t id, const struct spa_pod *param);
+		void (*add_buffer) (void *data, void *port_data, struct pw_buffer *buffer);
+		void (*remove_buffer) (void *data, void *port_data, struct pw_buffer *buffer);
+		void (*process) (void *data, struct spa_io_position *position);
 		void (*drained) (void *data);
 		void (*command) (void *data, const struct spa_command *command);
-		void (*trigger_done) (void *data);
-	} test = { PW_VERSION_STREAM_EVENTS, NULL };
+	} test = { PW_VERSION_FILTER_EVENTS, NULL };
 
-	struct pw_stream_events ev;
+	struct pw_filter_events ev;
 
 	TEST_FUNC(ev, test, destroy);
 	TEST_FUNC(ev, test, state_changed);
-	TEST_FUNC(ev, test, control_info);
 	TEST_FUNC(ev, test, io_changed);
 	TEST_FUNC(ev, test, param_changed);
 	TEST_FUNC(ev, test, add_buffer);
@@ -64,81 +61,72 @@ static void test_abi(void)
 	TEST_FUNC(ev, test, process);
 	TEST_FUNC(ev, test, drained);
 	TEST_FUNC(ev, test, command);
-	TEST_FUNC(ev, test, trigger_done);
 
-#if defined(__x86_64__) && defined(__LP64__)
-	spa_assert_se(sizeof(struct pw_buffer) == 32);
-	spa_assert_se(sizeof(struct pw_time) == 56);
-#else
-	fprintf(stderr, "%zd\n", sizeof(struct pw_buffer));
-	fprintf(stderr, "%zd\n", sizeof(struct pw_time));
-#endif
-
-	spa_assert_se(PW_VERSION_STREAM_EVENTS == 2);
+	spa_assert_se(PW_VERSION_FILTER_EVENTS == 1);
 	spa_assert_se(sizeof(ev) == sizeof(test));
 
-	spa_assert_se(PW_STREAM_STATE_ERROR == -1);
-	spa_assert_se(PW_STREAM_STATE_UNCONNECTED == 0);
-	spa_assert_se(PW_STREAM_STATE_CONNECTING == 1);
-	spa_assert_se(PW_STREAM_STATE_PAUSED == 2);
-	spa_assert_se(PW_STREAM_STATE_STREAMING == 3);
+	spa_assert_se(PW_FILTER_STATE_ERROR == -1);
+	spa_assert_se(PW_FILTER_STATE_UNCONNECTED == 0);
+	spa_assert_se(PW_FILTER_STATE_CONNECTING == 1);
+	spa_assert_se(PW_FILTER_STATE_PAUSED == 2);
+	spa_assert_se(PW_FILTER_STATE_STREAMING == 3);
 
-	spa_assert_se(pw_stream_state_as_string(PW_STREAM_STATE_ERROR) != NULL);
-	spa_assert_se(pw_stream_state_as_string(PW_STREAM_STATE_UNCONNECTED) != NULL);
-	spa_assert_se(pw_stream_state_as_string(PW_STREAM_STATE_CONNECTING) != NULL);
-	spa_assert_se(pw_stream_state_as_string(PW_STREAM_STATE_PAUSED) != NULL);
-	spa_assert_se(pw_stream_state_as_string(PW_STREAM_STATE_STREAMING) != NULL);
-}
-
-static void stream_destroy_error(void *data)
-{
-	spa_assert_not_reached();
-}
-static void stream_state_changed_error(void *data, enum pw_stream_state old,
-		enum pw_stream_state state, const char *error)
-{
-	spa_assert_not_reached();
-}
-static void stream_io_changed_error(void *data, uint32_t id, void *area, uint32_t size)
-{
-	spa_assert_not_reached();
-}
-static void stream_param_changed_error(void *data, uint32_t id, const struct spa_pod *format)
-{
-	spa_assert_not_reached();
-}
-static void stream_add_buffer_error(void *data, struct pw_buffer *buffer)
-{
-	spa_assert_not_reached();
-}
-static void stream_remove_buffer_error(void *data, struct pw_buffer *buffer)
-{
-	spa_assert_not_reached();
-}
-static void stream_process_error(void *data)
-{
-	spa_assert_not_reached();
-}
-static void stream_drained_error(void *data)
-{
-	spa_assert_not_reached();
+	spa_assert_se(pw_filter_state_as_string(PW_FILTER_STATE_ERROR) != NULL);
+	spa_assert_se(pw_filter_state_as_string(PW_FILTER_STATE_UNCONNECTED) != NULL);
+	spa_assert_se(pw_filter_state_as_string(PW_FILTER_STATE_CONNECTING) != NULL);
+	spa_assert_se(pw_filter_state_as_string(PW_FILTER_STATE_PAUSED) != NULL);
+	spa_assert_se(pw_filter_state_as_string(PW_FILTER_STATE_STREAMING) != NULL);
 }
 
-static const struct pw_stream_events stream_events_error =
+static void filter_destroy_error(void *data)
 {
-	PW_VERSION_STREAM_EVENTS,
-        .destroy = stream_destroy_error,
-        .state_changed = stream_state_changed_error,
-	.io_changed = stream_io_changed_error,
-	.param_changed = stream_param_changed_error,
-	.add_buffer = stream_add_buffer_error,
-	.remove_buffer = stream_remove_buffer_error,
-	.process = stream_process_error,
-	.drained = stream_drained_error
+	spa_assert_not_reached();
+}
+static void filter_state_changed_error(void *data, enum pw_filter_state old,
+		enum pw_filter_state state, const char *error)
+{
+	spa_assert_not_reached();
+}
+static void filter_io_changed_error(void *data, void *port_data, uint32_t id, void *area, uint32_t size)
+{
+	spa_assert_not_reached();
+}
+static void filter_param_changed_error(void *data, void *port_data, uint32_t id, const struct spa_pod *format)
+{
+	spa_assert_not_reached();
+}
+static void filter_add_buffer_error(void *data, void *port_data, struct pw_buffer *buffer)
+{
+	spa_assert_not_reached();
+}
+static void filter_remove_buffer_error(void *data, void *port_data, struct pw_buffer *buffer)
+{
+	spa_assert_not_reached();
+}
+static void filter_process_error(void *data, struct spa_io_position *position)
+{
+	spa_assert_not_reached();
+}
+static void filter_drained_error(void *data)
+{
+	spa_assert_not_reached();
+}
+
+static const struct pw_filter_events filter_events_error =
+{
+	PW_VERSION_FILTER_EVENTS,
+        .destroy = filter_destroy_error,
+        .state_changed = filter_state_changed_error,
+	.io_changed = filter_io_changed_error,
+	.param_changed = filter_param_changed_error,
+	.add_buffer = filter_add_buffer_error,
+	.remove_buffer = filter_remove_buffer_error,
+	.process = filter_process_error,
+	.drained = filter_drained_error
 };
 
 static int destroy_count = 0;
-static void stream_destroy_count(void *data)
+static void filter_destroy_count(void *data)
 {
 	destroy_count++;
 }
@@ -147,45 +135,33 @@ static void test_create(void)
 	struct pw_main_loop *loop;
 	struct pw_context *context;
 	struct pw_core *core;
-	struct pw_stream *stream;
-	struct pw_stream_events stream_events = stream_events_error;
+	struct pw_filter *filter;
+	struct pw_filter_events filter_events = filter_events_error;
 	struct spa_hook listener = { 0, };
 	const char *error = NULL;
-	struct pw_time tm;
 
 	loop = pw_main_loop_new(NULL);
 	context = pw_context_new(pw_main_loop_get_loop(loop), NULL, 12);
 	spa_assert_se(context != NULL);
 	core = pw_context_connect_self(context, NULL, 0);
 	spa_assert_se(core != NULL);
-	stream = pw_stream_new(core, "test", NULL);
-	spa_assert_se(stream != NULL);
-	pw_stream_add_listener(stream, &listener, &stream_events, stream);
+	filter = pw_filter_new(core, "test", NULL);
+	spa_assert_se(filter != NULL);
+	pw_filter_add_listener(filter, &listener, &filter_events, filter);
 
 	/* check state */
-	spa_assert_se(pw_stream_get_state(stream, &error) == PW_STREAM_STATE_UNCONNECTED);
+	spa_assert_se(pw_filter_get_state(filter, &error) == PW_FILTER_STATE_UNCONNECTED);
 	spa_assert_se(error == NULL);
 	/* check name */
-	spa_assert_se(spa_streq(pw_stream_get_name(stream), "test"));
+	spa_assert_se(spa_streq(pw_filter_get_name(filter), "test"));
 
 	/* check id, only when connected */
-	spa_assert_se(pw_stream_get_node_id(stream) == SPA_ID_INVALID);
-
-	spa_assert_se(pw_stream_get_time_n(stream, &tm, sizeof(tm)) == 0);
-	spa_assert_se(tm.now == 0);
-	spa_assert_se(tm.rate.num == 0);
-	spa_assert_se(tm.rate.denom == 0);
-	spa_assert_se(tm.ticks == 0);
-	spa_assert_se(tm.delay == 0);
-	spa_assert_se(tm.queued == 0);
-	spa_assert_se(tm.buffered == 0);
-
-	spa_assert_se(pw_stream_dequeue_buffer(stream) == NULL);
+	spa_assert_se(pw_filter_get_node_id(filter) == SPA_ID_INVALID);
 
 	/* check destroy */
 	destroy_count = 0;
-	stream_events.destroy = stream_destroy_count;
-	pw_stream_destroy(stream);
+	filter_events.destroy = filter_destroy_count;
+	pw_filter_destroy(filter);
 	spa_assert_se(destroy_count == 1);
 
 	pw_context_destroy(context);
@@ -198,8 +174,8 @@ static void test_properties(void)
 	struct pw_context *context;
 	struct pw_core *core;
 	const struct pw_properties *props;
-	struct pw_stream *stream;
-	struct pw_stream_events stream_events = stream_events_error;
+	struct pw_filter *filter;
+	struct pw_filter_events filter_events = filter_events_error;
 	struct spa_hook listener = { { NULL }, };
 	struct spa_dict_item items[3];
 
@@ -208,14 +184,14 @@ static void test_properties(void)
 	spa_assert_se(context != NULL);
 	core = pw_context_connect_self(context, NULL, 0);
 	spa_assert_se(core != NULL);
-	stream = pw_stream_new(core, "test",
+	filter = pw_filter_new(core, "test",
 			pw_properties_new("foo", "bar",
 					  "biz", "fuzz",
 					  NULL));
-	spa_assert_se(stream != NULL);
-	pw_stream_add_listener(stream, &listener, &stream_events, stream);
+	spa_assert_se(filter != NULL);
+	pw_filter_add_listener(filter, &listener, &filter_events, filter);
 
-	props = pw_stream_get_properties(stream);
+	props = pw_filter_get_properties(filter, NULL);
 	spa_assert_se(props != NULL);
 	spa_assert_se(spa_streq(pw_properties_get(props, "foo"), "bar"));
 	spa_assert_se(spa_streq(pw_properties_get(props, "biz"), "fuzz"));
@@ -227,16 +203,16 @@ static void test_properties(void)
 	items[1] = SPA_DICT_ITEM_INIT("biz", "buzz");
 	/* add buzz */
 	items[2] = SPA_DICT_ITEM_INIT("buzz", "frizz");
-	pw_stream_update_properties(stream, &SPA_DICT_INIT(items, 3));
+	pw_filter_update_properties(filter, NULL, &SPA_DICT_INIT(items, 3));
 
-	spa_assert_se(props == pw_stream_get_properties(stream));
+	spa_assert_se(props == pw_filter_get_properties(filter, NULL));
 	spa_assert_se(pw_properties_get(props, "foo") == NULL);
 	spa_assert_se(spa_streq(pw_properties_get(props, "biz"), "buzz"));
 	spa_assert_se(spa_streq(pw_properties_get(props, "buzz"), "frizz"));
 
 	/* check destroy */
 	destroy_count = 0;
-	stream_events.destroy = stream_destroy_count;
+	filter_events.destroy = filter_destroy_count;
 	pw_context_destroy(context);
 	spa_assert_se(destroy_count == 1);
 

5fbda82cc376c76d12ffdba36de694d95feefaf1
Wim Taymans <wtaymans@redhat.com>
2022-07-09 20:35:47 +0200

    spa: tweak SPA_MIN and SPA_MAX
    This generates much better assembler because it can use a cmov in
    more cases.

--- spa/include/spa/utils/defs.h
+++ spa/include/spa/utils/defs.h
@@ -131,13 +131,13 @@ struct spa_fraction {
 ({				\
 	__typeof__(a) _min_a = (a);	\
 	__typeof__(b) _min_b = (b);	\
-	SPA_LIKELY(_min_a < _min_b) ? _min_a : _min_b;	\
+	SPA_LIKELY(_min_a <= _min_b) ? _min_a : _min_b;	\
 })
 #define SPA_MAX(a,b)		\
 ({				\
 	__typeof__(a) _max_a = (a);	\
 	__typeof__(b) _max_b = (b);	\
-	SPA_LIKELY(_max_a > _max_b) ? _max_a : _max_b;	\
+	SPA_LIKELY(_max_a >= _max_b) ? _max_a : _max_b;	\
 })
 #define SPA_CLAMP(v,low,high)				\
 ({							\

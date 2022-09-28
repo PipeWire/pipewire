38e8e76f76be95c30facce6f4427e708d33b2d9c
Wim Taymans <wtaymans@redhat.com>
2022-09-28 09:13:31 +0200

    modules: disconnect streams before destroy
    So that they are both stopped before being destroyed.

--- src/modules/module-filter-chain.c
+++ src/modules/module-filter-chain.c
@@ -2080,12 +2080,20 @@ static const struct pw_proxy_events core_proxy_events = {
 
 static void impl_destroy(struct impl *impl)
 {
+	/* disconnect both streams before destroying any of them */
+	if (impl->capture)
+		pw_stream_disconnect(impl->capture);
+	if (impl->playback)
+		pw_stream_disconnect(impl->playback);
+
 	if (impl->capture)
 		pw_stream_destroy(impl->capture);
 	if (impl->playback)
 		pw_stream_destroy(impl->playback);
+
 	if (impl->core && impl->do_disconnect)
 		pw_core_disconnect(impl->core);
+
 	pw_properties_free(impl->capture_props);
 	pw_properties_free(impl->playback_props);
 	graph_free(&impl->graph);
--- src/modules/module-loopback.c
+++ src/modules/module-loopback.c
@@ -414,8 +414,10 @@ static void impl_destroy(struct impl *impl)
 		pw_stream_destroy(impl->capture);
 	if (impl->playback)
 		pw_stream_destroy(impl->playback);
+
 	if (impl->core && impl->do_disconnect)
 		pw_core_disconnect(impl->core);
+
 	pw_properties_free(impl->capture_props);
 	pw_properties_free(impl->playback_props);
 	free(impl);

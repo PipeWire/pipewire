8d6ff6137df8b28fcef05264277bd5a4f697b2ff
Wim Taymans <wtaymans@redhat.com>
2022-11-09 16:29:04 +0100

    audioadapter: remove checks for format
    This is meant to be a filter for the follower format and can thus
    contain wildcards.

--- spa/plugins/audioconvert/audioadapter.c
+++ spa/plugins/audioconvert/audioadapter.c
@@ -579,10 +579,6 @@ static int impl_node_set_param(void *object, uint32_t id, uint32_t flags,
 				return -EINVAL;
 		if (spa_format_audio_raw_parse(param, &info.info.raw) < 0)
 			return -EINVAL;
-		if (info.info.raw.format == 0 ||
-		    info.info.raw.rate == 0 ||
-		    info.info.raw.channels == 0)
-			return -EINVAL;
 
 		this->follower_current_format = info;
 		break;

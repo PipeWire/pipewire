89d4cafec4e9ade45a278d3fa067c9ae8270a077
Wim Taymans <wtaymans@redhat.com>
2022-12-06 19:02:02 +0100

    pulse-server: fix module args

--- src/modules/module-protocol-pulse/modules/module-echo-cancel.c
+++ src/modules/module-protocol-pulse/modules/module-echo-cancel.c
@@ -63,7 +63,7 @@ static const struct pw_impl_module_events module_events = {
 	.destroy = module_destroy
 };
 
-static int module_echo_cancel_load(struct client *client, struct module *module)
+static int module_echo_cancel_load(struct module *module)
 {
 	struct module_echo_cancel_data *data = module->user_data;
 	FILE *f;

714b43881473481e13288b50514180aacdbae264
Robert Mader <robert.mader@collabora.com>
2023-01-11 23:20:19 +0100

    libcamera: Check format info for correct flags and modifier

--- spa/plugins/libcamera/libcamera-source.cpp
+++ spa/plugins/libcamera/libcamera-source.cpp
@@ -676,7 +676,10 @@ static int port_set_format(struct impl *impl, struct port *port,
 			    info.media_subtype == port->current_format->media_subtype &&
 			    info.info.raw.format == port->current_format->info.raw.format &&
 			    info.info.raw.size.width == port->current_format->info.raw.size.width &&
-			    info.info.raw.size.height == port->current_format->info.raw.size.height)
+			    info.info.raw.size.height == port->current_format->info.raw.size.height &&
+			    info.info.raw.flags == port->current_format->info.raw.flags &&
+			    (!(info.info.raw.flags & SPA_VIDEO_FLAG_MODIFIER) ||
+			     (info.info.raw.modifier == port->current_format->info.raw.modifier)))
 				return 0;
 			break;
 		case SPA_MEDIA_SUBTYPE_mjpg:

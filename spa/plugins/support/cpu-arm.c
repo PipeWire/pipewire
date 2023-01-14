PipeWire/pipewire

commit da26563a83961e2aa738141b4e0140d7796fc78c

cpu: clean up
Change read_file to spa_cpu_read_file and remove static because it might
not be used for some cpus.
Use spa_cpu_read_file in arm cpu detection.

spa/plugins/support/cpu-arm.c
@@ -49,37 +49,14 @@ static char *get_cpuinfo_line(char *cpuinfo, const char *tag)
 	return strndup(colon, end - colon);
 }
 
-static char *get_cpuinfo(void)
-{
-	char *cpuinfo;
-	int n, fd;
-
-	cpuinfo = malloc(MAX_BUFFER);
-
-	if ((fd = open("/proc/cpuinfo", O_RDONLY | O_CLOEXEC, 0)) < 0) {
-		free(cpuinfo);
-		return NULL;
-	}
-
-	if ((n = read(fd, cpuinfo, MAX_BUFFER-1)) < 0) {
-		free(cpuinfo);
-		close(fd);
-		return NULL;
-	}
-	cpuinfo[n] = 0;
-	close(fd);
-
-	return cpuinfo;
-}
-
 static int
 arm_init(struct impl *impl)
 {
 	uint32_t flags = 0;
-	char *cpuinfo, *line;
+	char *cpuinfo, *line, buffer[MAX_BUFFER];
 	int arch;
 
-	if (!(cpuinfo = get_cpuinfo())) {
+	if (!(cpuinfo = spa_cpu_read_file("/proc/cpuinfo", buffer, sizeof(buffer)))) {
 		spa_log_warn(impl->log, "%p: Can't read cpuinfo", impl);
 		return 1;
 	}

spa/plugins/support/cpu.c
@@ -59,7 +59,7 @@ struct impl {
 	uint32_t vm_type;
 };
 
-static char *read_file(const char *name, char *buffer, size_t len)
+char *spa_cpu_read_file(const char *name, char *buffer, size_t len)
 {
 	int n, fd;
 
@@ -175,7 +175,7 @@ impl_cpu_get_vm_type(void *object)
 	SPA_FOR_EACH_ELEMENT_VAR(dmi_vendors, dv) {
 		char buffer[256], *s;
 
-		if ((s = read_file(*dv, buffer, sizeof(buffer))) == NULL)
+		if ((s = spa_cpu_read_file(*dv, buffer, sizeof(buffer))) == NULL)
 			continue;
 
 		SPA_FOR_EACH_ELEMENT_VAR(dmi_vendor_table, t) {

commit 65f3a9c481214ce015d20cee813dd9dac2de8dab

cpu-arm: Fix incorrect free
A bug was introduced with the refactoring in da26563. In arm_init,
the buffer passed to spa_cpu_read_file is allocated and it is just
going to return that. So cpuinfo will actually point to buffer on
the stack, which need not be freed with a call to free.

The crash resulting with the incorrect free.

root@dragonboard-845c:~# pipewire --version
munmap_chunk(): invalid pointer
[  185.037284] audit: type=1701 audit(1659949975.843:14): auid=4294967295 uid=0 gid=0 ses=4294967295 pid=2243 comm="pipewire" exe="/usr/bin/pipewire" sig=6 res=1
Aborted
root@dragonboard-845c:~# wireplumber --version
munmap_chunk(): invalid pointer
[  193.453693] audit: type=1701 audit(1659949984.255:15): auid=4294967295 uid=0 gid=0 ses=4294967295 pid=2244 comm="wireplumber" exe="/usr/bin/wireplumber" sig=6 res=1
Aborted

Backtrace from the crash

(gdb) bt
!0  __pthread_kill_implementation (threadid=<optimized out>, signo=signo@entry=6, no_tid=no_tid@entry=0) at pthread_kill.c:44
!1  0x0000fffff7d8edd8 in __pthread_kill_internal (signo=6, threadid=<optimized out>) at pthread_kill.c:78
!2  0x0000fffff7d4a390 in __GI_raise (sig=sig@entry=6) at /usr/src/debug/glibc/2.36-r0/sysdeps/posix/raise.c:26
!3  0x0000fffff7d37498 in __GI_abort () at abort.c:79
!4  0x0000fffff7d83374 in __libc_message (action=action@entry=do_abort, fmt=fmt@entry=0xfffff7e5fc20 "%s\n") at /usr/src/debug/glibc/2.36-r0/sysdeps/posix/libc_fatal.c:155
!5  0x0000fffff7d988c0 in malloc_printerr (str=str@entry=0xfffff7e5a7f0 "munmap_chunk(): invalid pointer") at malloc.c:5660
!6  0x0000fffff7d98aac in munmap_chunk (p=p@entry=0xffffffffd258) at malloc.c:3054
!7  0x0000fffff7d9d068 in __GI___libc_free (mem=mem@entry=0xffffffffd268) at malloc.c:3375
!8  0x0000fffff7cd36cc in arm_init (impl=impl@entry=0xaaaaaaac8c48) at /usr/src/debug/pipewire/1.0-r0/spa/plugins/support/cpu-arm.c:97

spa/plugins/support/cpu-arm.c
@@ -94,8 +94,6 @@ arm_init(struct impl *impl)
 		free(line);
 	}
 
-	free(cpuinfo);
-
 	impl->flags = flags;
 
 	return 0;
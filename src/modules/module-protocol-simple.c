d776d378cdfa3add47670978a63eed28537cb12c
Barnabás Pőcze <pobrn@protonmail.com>
2023-02-25 20:21:45 +0100

    pipewire: module-protocol-simple: cast argument of `accept4()`
    With glibc omitting the cast does not trigger a warning because
    glibc uses GCC's `transparent_union` attribute. musl does not
    do that, so to avoid getting warnings there, add the cast.
    
    https://gcc.gnu.org/onlinedocs/gcc/Common-Type-Attributes.html#index-transparent_005funion-type-attribute

--- src/modules/module-protocol-simple.c
+++ src/modules/module-protocol-simple.c
@@ -523,7 +523,7 @@ on_connect(void *data, int fd, uint32_t mask)
 	struct pw_properties *props = NULL;
 
 	addrlen = sizeof(addr);
-	client_fd = accept4(fd, &addr, &addrlen, SOCK_NONBLOCK | SOCK_CLOEXEC);
+	client_fd = accept4(fd, (struct sockaddr *) &addr, &addrlen, SOCK_NONBLOCK | SOCK_CLOEXEC);
 	if (client_fd < 0)
 		goto error;
 

fd358c511d5e245b54ed4444116bf9487edf7d63
Wim Taymans <wtaymans@redhat.com>
2023-06-20 20:45:42 +0200

    context: a node is runnable only when active
    Also check if the node is active to decide if the node is runnable.

--- src/pipewire/context.c
+++ src/pipewire/context.c
@@ -1245,7 +1245,7 @@ again:
 	/* clean up the flags first */
 	spa_list_for_each(n, &context->node_list, link) {
 		n->visited = false;
-		n->runnable = n->always_process;
+		n->runnable = n->always_process && n->active;
 	}
 
 	get_quantums(context, &def_quantum, &min_quantum, &max_quantum, &lim_quantum, &rate_quantum);

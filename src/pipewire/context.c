4df753d0d7af7e68ab16ee90b94c4ddd44506074
Wim Taymans <wtaymans@redhat.com>
2023-06-26 16:11:33 +0200

    context: avoid segfault when no fallback driver
    In some cases, there might not be a fallback driver. Handle this without
    segfaulting.

--- src/pipewire/context.c
+++ src/pipewire/context.c
@@ -1327,11 +1327,11 @@ again:
 			if ((t->want_driver && t->active && t->runnable) ||
 			    t->always_process) {
 				driver = target;
-				driver->runnable = true;
 				break;
 			}
 		}
 		if (driver != NULL) {
+			driver->runnable = true;
 			/* driver needed for this group */
 			move_to_driver(context, &collect, driver);
 		} else {

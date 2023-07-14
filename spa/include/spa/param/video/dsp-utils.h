0a13d37e5cd999446a579683e94939c30c233f56
columbarius <co1umbarius@protonmail.com>
2023-07-14 03:36:18 +0200

    spa: Add SPA_VIDEO_FLAG_MODIFIER_FIXATION_REQUIRED
    This flags allows clients to use the parse_format functions and
    determine from their result directly if the modifer was fixated yet.

--- spa/include/spa/param/video/dsp-utils.h
+++ spa/include/spa/param/video/dsp-utils.h
@@ -23,8 +23,11 @@ spa_format_video_dsp_parse(const struct spa_pod *format,
 			   struct spa_video_info_dsp *info)
 {
 	info->flags = SPA_VIDEO_FLAG_NONE;
-	if (spa_pod_find_prop (format, NULL, SPA_FORMAT_VIDEO_modifier)) {
+	const struct spa_pod_prop *mod_prop;
+	if ((mod_prop = spa_pod_find_prop (format, NULL, SPA_FORMAT_VIDEO_modifier)) != NULL) {
 		info->flags |= SPA_VIDEO_FLAG_MODIFIER;
+		if ((mod_prop->flags & SPA_POD_PROP_FLAG_DONT_FIXATE) == SPA_POD_PROP_FLAG_DONT_FIXATE)
+			info->flags |= SPA_VIDEO_FLAG_MODIFIER_FIXATION_REQUIRED;
 	}
 
 	return spa_pod_parse_object(format,

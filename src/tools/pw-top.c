963ea1f57c04d09122d2d1b96317cfbbf15a8ece
Pauli Virtanen <pav@iki.fi>
2023-10-19 23:36:27 +0300

    tools: make pw-dump/mon/top try to connect to the default manager socket
    These tools are used for debugging, and should default to connecting to
    the manager socket if present.

--- src/tools/pw-top.c
+++ src/tools/pw-top.c
@@ -836,7 +836,8 @@ int main(int argc, char *argv[])
 
 	data.core = pw_context_connect(data.context,
 			pw_properties_new(
-				PW_KEY_REMOTE_NAME, opt_remote,
+				PW_KEY_REMOTE_NAME, opt_remote ? opt_remote :
+					("[" PW_DEFAULT_REMOTE "-manager," PW_DEFAULT_REMOTE "]"),
 				NULL),
 			0);
 	if (data.core == NULL) {

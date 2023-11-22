8c3cfab17b5d4fa390116331c358d9683d00ed14
Sergio Costas Rodriguez <sergio.costas@canonical.com>
2023-11-22 13:51:13 +0100

    Replace even more spaces with tabs

--- src/modules/module-protocol-pulse/snap-policy.h
+++ src/modules/module-protocol-pulse/snap-policy.h
@@ -6,11 +6,11 @@
 #define _SNAP_POLICY_H_
 
 typedef enum _pw_sandbox_access {
-    PW_SANDBOX_ACCESS_NONE           = 0,
-    PW_SANDBOX_ACCESS_NOT_A_SANDBOX  = 1 << 0,
-    PW_SANDBOX_ACCESS_RECORD         = 1 << 1,
-    PW_SANDBOX_ACCESS_PLAYBACK       = 1 << 2,
-    PW_SANDBOX_ACCESS_ALL            = (PW_SANDBOX_ACCESS_PLAYBACK | PW_SANDBOX_ACCESS_RECORD),
+	PW_SANDBOX_ACCESS_NONE           = 0,
+	PW_SANDBOX_ACCESS_NOT_A_SANDBOX  = 1 << 0,
+	PW_SANDBOX_ACCESS_RECORD         = 1 << 1,
+	PW_SANDBOX_ACCESS_PLAYBACK       = 1 << 2,
+	PW_SANDBOX_ACCESS_ALL            = (PW_SANDBOX_ACCESS_PLAYBACK | PW_SANDBOX_ACCESS_RECORD),
 } pw_sandbox_access_t;
 
 #define PW_KEY_SNAP_ID "pipewire.snap.id"

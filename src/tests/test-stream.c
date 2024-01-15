a1ecfc8d7ff2c60a4ea4c3f874c2f9968d4d59b8
Wim Taymans <wtaymans@redhat.com>
2024-01-15 15:53:19 +0100

    tests: fix ABI check for new pw_time

--- src/tests/test-stream.c
+++ src/tests/test-stream.c
@@ -48,7 +48,7 @@ static void test_abi(void)
 
 #if defined(__x86_64__) && defined(__LP64__)
 	spa_assert_se(sizeof(struct pw_buffer) == 32);
-	spa_assert_se(sizeof(struct pw_time) == 56);
+	spa_assert_se(sizeof(struct pw_time) == 64);
 #else
 	fprintf(stderr, "%zd\n", sizeof(struct pw_buffer));
 	fprintf(stderr, "%zd\n", sizeof(struct pw_time));

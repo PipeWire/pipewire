3a4374ec096dfba10d188764d892f32ab97eab8c
Wim Taymans <wtaymans@redhat.com>
2024-01-26 13:28:07 +0100

    meson: fix build with missing libudev-devel
    (cherry picked from commit b4c7973d495daaa6e011a0ba1a87274252e7b71e)

--- spa/plugins/v4l2/v4l2.c
+++ spa/plugins/v4l2/v4l2.c
@@ -27,8 +27,13 @@ int spa_handle_factory_enum(const struct spa_handle_factory **factory,
 		*factory = &spa_v4l2_source_factory;
 		break;
 	case 1:
+#ifdef HAVE_LIBUDEV
 		*factory = &spa_v4l2_udev_factory;
 		break;
+#else
+		(*index)++;
+		SPA_FALLTHROUGH;
+#endif
 	case 2:
 		*factory = &spa_v4l2_device_factory;
 		break;

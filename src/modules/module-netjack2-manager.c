86ae379e55f9ee21951a24a5e0db9d37557269b0
Gleb Popov <6yearold@gmail.com>
2024-02-10 18:08:34 +0300

    Fix building module-netjack2 on FreeBSD

--- src/modules/module-netjack2-manager.c
+++ src/modules/module-netjack2-manager.c
@@ -37,6 +37,10 @@
 
 #include "module-netjack2/peer.c"
 
+#ifdef __FreeBSD__
+#define ifr_ifindex ifr_index
+#endif
+
 #ifndef IPTOS_DSCP
 #define IPTOS_DSCP_MASK 0xfc
 #define IPTOS_DSCP(x) ((x) & IPTOS_DSCP_MASK)

d2750420c6ea449c4bd60d35826174c1a3e79426
Gleb Popov <6yearold@gmail.com>
2024-02-10 19:32:49 +0300

    Fix build of vulkan/dmabuf_fallback.c

--- spa/plugins/vulkan/dmabuf_fallback.c
+++ spa/plugins/vulkan/dmabuf_fallback.c
@@ -32,11 +32,11 @@ bool dmabuf_check_sync_file_import_export(struct spa_log *log) {
 }
 
 bool dmabuf_import_sync_file(struct spa_log *log, int dmabuf_fd, uint32_t flags, int sync_file_fd) {
-	spa_log_error("DMA-BUF sync_file import IOCTL not available on this system");
+	spa_log_error(log, "DMA-BUF sync_file import IOCTL not available on this system");
 	return false;
 }
 
 int dmabuf_export_sync_file(struct spa_log *log, int dmabuf_fd, uint32_t flags) {
-	spa_log_error("DMA-BUF sync_file export IOCTL not available on this system");
+	spa_log_error(log, "DMA-BUF sync_file export IOCTL not available on this system");
 	return false;
 }

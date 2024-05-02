f81f2f8d38fc652ef4adcb0557ecc2bc5c6dde31
Barnabás Pőcze <pobrn@protonmail.com>
2024-05-02 03:24:35 +0200

    treewide: fix C++20 compilation error wrt. designated initializers
    C++20 introduced designated initializers similar to the ones found
    in C99, however, in C++ designated initializers cannot be mixed
    with non-designated initializers. GCC rejects mixed initializers
    with an error.

--- spa/include/spa/pod/dynamic.h
+++ spa/include/spa/pod/dynamic.h
@@ -41,7 +41,7 @@ static inline void spa_pod_dynamic_builder_init(struct spa_pod_dynamic_builder *
 		void *data, uint32_t size, uint32_t extend)
 {
 	static const struct spa_pod_builder_callbacks spa_pod_dynamic_builder_callbacks = {
-		SPA_VERSION_POD_BUILDER_CALLBACKS,
+		.version = SPA_VERSION_POD_BUILDER_CALLBACKS,
 		.overflow = spa_pod_dynamic_builder_overflow
 	};
 	builder->b = SPA_POD_BUILDER_INIT(data, size);

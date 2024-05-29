PipeWire/pipewire

commit f400ff20504d739a520bb7b57543b9203bc7ad9c

gst: Check for video/ caps before parsing for info
The gst_video_info_from_caps() API isn't really intended to be used as a
check-for-videoness function (it generates an error-level GStreamer
debug message when used this way).

We check the caps for a video/ name for this reason, which is
functionally equivalent.

src/gst/gstpipewiresrc.c
@@ -1028,6 +1028,11 @@ handle_format_change (GstPipeWireSrc *pwsrc,
   if (pwsrc->caps && gst_caps_is_fixed (pwsrc->caps)) {
     pwsrc->negotiated = TRUE;
 
+    if (g_str_has_prefix (gst_structure_get_name (
+            gst_caps_get_structure (pwsrc->caps, 0)),
+          "video/")) {
+      pwsrc->is_video = TRUE;
+
 #ifdef HAVE_GSTREAMER_DMA_DRM
     if (gst_video_is_dma_drm_caps (pwsrc->caps)) {
       if (!gst_video_info_dma_drm_from_caps (&pwsrc->drm_info, pwsrc->caps)) {
@@ -1042,13 +1047,12 @@ handle_format_change (GstPipeWireSrc *pwsrc,
         pw_stream_set_error (pwsrc->stream, -EINVAL, "internal error");
         return;
       }
-
-      pwsrc->is_video = TRUE;
     } else {
       gst_video_info_dma_drm_init (&pwsrc->drm_info);
 #endif
-      pwsrc->is_video = gst_video_info_from_caps (&pwsrc->video_info,
-                                                  pwsrc->caps);
+      gst_video_info_from_caps (&pwsrc->video_info,
+          pwsrc->caps);
+      }
 #ifdef HAVE_GSTREAMER_DMA_DRM
     }
 #endif

src/gst/gstpipewirepool.c
@@ -224,7 +224,14 @@ set_config (GstBufferPool * pool, GstStructure * config)
     return FALSE;
   }
 
-  has_video = gst_video_info_from_caps (&p->video_info, caps);
+  if (g_str_has_prefix (gst_structure_get_name (
+          gst_caps_get_structure (caps, 0)),
+        "video/")) {
+    has_video = TRUE;
+    gst_video_info_from_caps (&p->video_info, caps);
+  } else {
+    has_video = FALSE;
+  }
 
   p->add_metavideo = has_video && gst_buffer_pool_config_has_option (config,
       GST_BUFFER_POOL_OPTION_VIDEO_META);

commit adead74e8c116cb486551a8e7ab4c45752adb7eb

gst: src: Indentation fix
Fixes: f400ff205 (gst: Check for video/ caps before parsing for info)

src/gst/gstpipewiresrc.c
@@ -1045,24 +1045,24 @@ handle_format_change (GstPipeWireSrc *pwsrc,
       pwsrc->is_video = TRUE;
 
 #ifdef HAVE_GSTREAMER_DMA_DRM
-    if (gst_video_is_dma_drm_caps (pwsrc->caps)) {
-      if (!gst_video_info_dma_drm_from_caps (&pwsrc->drm_info, pwsrc->caps)) {
-        GST_WARNING_OBJECT (pwsrc, "Can't create drm video info from caps");
-        pw_stream_set_error (pwsrc->stream, -EINVAL, "internal error");
-        return;
-      }
+      if (gst_video_is_dma_drm_caps (pwsrc->caps)) {
+        if (!gst_video_info_dma_drm_from_caps (&pwsrc->drm_info, pwsrc->caps)) {
+          GST_WARNING_OBJECT (pwsrc, "Can't create drm video info from caps");
+          pw_stream_set_error (pwsrc->stream, -EINVAL, "internal error");
+          return;
+        }
 
-      if (!gst_video_info_dma_drm_to_video_info (&pwsrc->drm_info,
-                                                 &pwsrc->video_info)) {
-        GST_WARNING_OBJECT (pwsrc, "Can't create video info from drm video info");
-        pw_stream_set_error (pwsrc->stream, -EINVAL, "internal error");
-        return;
-      }
-    } else {
-      gst_video_info_dma_drm_init (&pwsrc->drm_info);
+        if (!gst_video_info_dma_drm_to_video_info (&pwsrc->drm_info,
+                                                   &pwsrc->video_info)) {
+          GST_WARNING_OBJECT (pwsrc, "Can't create video info from drm video info");
+          pw_stream_set_error (pwsrc->stream, -EINVAL, "internal error");
+          return;
+        }
+      } else {
+        gst_video_info_dma_drm_init (&pwsrc->drm_info);
 #endif
-      gst_video_info_from_caps (&pwsrc->video_info,
-          pwsrc->caps);
+        gst_video_info_from_caps (&pwsrc->video_info,
+            pwsrc->caps);
       }
 #ifdef HAVE_GSTREAMER_DMA_DRM
     }
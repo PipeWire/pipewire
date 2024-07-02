PipeWire/pipewire

commit 0c40c014775e3e155ba1381308d8361dd660e1a6

gst: factor out the stream management and some common variables in a new class
Construct this new class from both the src and sink to be able to share code

Consolidate the previous mess of open/close/start/stop into a single pair
of open/close functions in the new stream class

src/gst/gstpipewirestream.c
@@ -1,0 +1,168 @@
+/* GStreamer */
+/* SPDX-FileCopyrightText: Copyright © 2018 Wim Taymans */
+/* SPDX-FileCopyrightText: Copyright © 2024 Collabora Ltd. */
+/* SPDX-License-Identifier: MIT */
+
+#include "gstpipewirestream.h"
+
+GST_DEBUG_CATEGORY_STATIC (pipewire_stream_debug);
+#define GST_CAT_DEFAULT pipewire_stream_debug
+
+G_DEFINE_TYPE (GstPipeWireStream, gst_pipewire_stream, GST_TYPE_OBJECT)
+
+static void
+gst_pipewire_stream_init (GstPipeWireStream * self)
+{
+  self->fd = -1;
+  self->client_name = g_strdup (pw_get_client_name());
+  self->pool = gst_pipewire_pool_new ();
+}
+
+static void
+gst_pipewire_stream_finalize (GObject * object)
+{
+  GstPipeWireStream * self = GST_PIPEWIRE_STREAM (object);
+
+  g_clear_object (&self->pool);
+  g_free (self->path);
+  g_free (self->target_object);
+  g_free (self->client_name);
+  gst_clear_structure (&self->client_properties);
+  gst_clear_structure (&self->stream_properties);
+
+  G_OBJECT_CLASS(gst_pipewire_stream_parent_class)->finalize (object);
+}
+
+void
+gst_pipewire_stream_class_init (GstPipeWireStreamClass * klass)
+{
+  GObjectClass *gobject_class = G_OBJECT_CLASS (klass);
+
+  gobject_class->finalize = gst_pipewire_stream_finalize;
+
+  GST_DEBUG_CATEGORY_INIT (pipewire_stream_debug, "pipewirestream", 0,
+      "PipeWire Stream");
+}
+
+GstPipeWireStream *
+gst_pipewire_stream_new (GstElement * element)
+{
+  GstPipeWireStream *stream;
+
+  stream = g_object_new (GST_TYPE_PIPEWIRE_STREAM, NULL);
+  stream->element = element;
+
+  return stream;
+}
+
+static gboolean
+copy_properties (GQuark field_id,
+                 const GValue *value,
+                 gpointer user_data)
+{
+  struct pw_properties *properties = user_data;
+  GValue dst = { 0 };
+
+  if (g_value_type_transformable (G_VALUE_TYPE(value), G_TYPE_STRING)) {
+    g_value_init (&dst, G_TYPE_STRING);
+    if (g_value_transform (value, &dst)) {
+      pw_properties_set (properties,
+                         g_quark_to_string (field_id),
+                         g_value_get_string (&dst));
+    }
+    g_value_unset (&dst);
+  }
+  return TRUE;
+}
+
+gboolean
+gst_pipewire_stream_open (GstPipeWireStream * self,
+    const struct pw_stream_events * pwstream_events)
+{
+  struct pw_properties *props;
+
+  g_return_val_if_fail (self->core == NULL, FALSE);
+
+  GST_DEBUG_OBJECT (self, "open");
+
+  /* acquire the core */
+  self->core = gst_pipewire_core_get (self->fd);
+  if (self->core == NULL)
+      goto connect_error;
+
+  pw_thread_loop_lock (self->core->loop);
+
+  /* update the client properties */
+  if (self->client_properties) {
+    props = pw_properties_new (NULL, NULL);
+    gst_structure_foreach (self->client_properties, copy_properties, props);
+    pw_core_update_properties (self->core->core, &props->dict);
+    pw_properties_free (props);
+  }
+
+  /* create stream */
+  props = pw_properties_new (NULL, NULL);
+  if (self->client_name) {
+    pw_properties_set (props, PW_KEY_NODE_NAME, self->client_name);
+    pw_properties_set (props, PW_KEY_NODE_DESCRIPTION, self->client_name);
+  }
+  if (self->stream_properties) {
+    gst_structure_foreach (self->stream_properties, copy_properties, props);
+  }
+
+  if ((self->pwstream = pw_stream_new (self->core->core,
+                                       self->client_name, props)) == NULL)
+    goto no_stream;
+
+  pw_stream_add_listener(self->pwstream,
+                         &self->pwstream_listener,
+                         pwstream_events,
+                         self->element);
+
+  /* create clock */
+  self->clock = gst_pipewire_clock_new (self->pwstream, 0);
+
+  self->pool->stream = self->pwstream;
+
+  pw_thread_loop_unlock (self->core->loop);
+
+  return TRUE;
+
+  /* ERRORS */
+connect_error:
+  {
+    GST_ELEMENT_ERROR (self->element, RESOURCE, FAILED,
+        ("Failed to connect"), (NULL));
+    return FALSE;
+  }
+no_stream:
+  {
+    GST_ELEMENT_ERROR (self->element, RESOURCE, FAILED,
+        ("can't create stream"), (NULL));
+    pw_thread_loop_unlock (self->core->loop);
+    return FALSE;
+  }
+}
+
+void
+gst_pipewire_stream_close (GstPipeWireStream * self)
+{
+  GST_DEBUG_OBJECT (self, "close");
+
+  self->pool->stream = NULL;
+
+  /* destroy the clock */
+  gst_element_post_message (GST_ELEMENT (self->element),
+    gst_message_new_clock_lost (GST_OBJECT_CAST (self->element), self->clock));
+
+  GST_PIPEWIRE_CLOCK (self->clock)->stream = NULL;
+  g_clear_object (&self->clock);
+
+  /* destroy the pw stream */
+  pw_thread_loop_lock (self->core->loop);
+  g_clear_pointer (&self->pwstream, pw_stream_destroy);
+  pw_thread_loop_unlock (self->core->loop);
+
+  /* release the core */
+  g_clear_pointer (&self->core, gst_pipewire_core_release);
+}

src/gst/gstpipewiresink.h
@@ -5,6 +5,8 @@
 #ifndef __GST_PIPEWIRE_SINK_H__
 #define __GST_PIPEWIRE_SINK_H__
 
+#include "gstpipewirestream.h"
+
 #include <gst/gst.h>
 #include <gst/base/gstbasesink.h>
 
@@ -57,28 +59,12 @@ struct _GstPipeWireSink {
   GstBaseSink element;
 
   /*< private >*/
-  gchar *path;
-  gchar *target_object;
-  gchar *client_name;
-  int fd;
+  GstPipeWireStream *stream;
 
   /* video state */
   gboolean negotiated;
 
-  GstPipeWireCore *core;
-  struct spa_hook core_listener;
-  GstStructure *client_properties;
-
-  struct pw_stream *stream;
-  struct spa_hook stream_listener;
-
-  GstStructure *stream_properties;
   GstPipeWireSinkMode mode;
-
-  GstPipeWirePool *pool;
-
-  GstClock *clock;
-  GstClockTime last_time;
 };
 
 struct _GstPipeWireSinkClass {

src/gst/gstpipewiresrc.h
@@ -7,6 +7,8 @@
 
 #include "config.h"
 
+#include "gstpipewirestream.h"
+
 #include <gst/gst.h>
 #include <gst/base/gstpushsrc.h>
 
@@ -42,14 +44,12 @@ typedef struct _GstPipeWireSrcClass GstPipeWireSrcClass;
 struct _GstPipeWireSrc {
   GstPushSrc element;
 
+  GstPipeWireStream *stream;
+
   /*< private >*/
-  gchar *path;
-  gchar *target_object;
-  gchar *client_name;
   gboolean always_copy;
   gint min_buffers;
   gint max_buffers;
-  int fd;
   gboolean resend_last;
   gint keepalive_time;
   gboolean autoconnect;
@@ -73,21 +73,7 @@ struct _GstPipeWireSrc {
   GstClockTime min_latency;
   GstClockTime max_latency;
 
-  GstStructure *client_properties;
-  GstPipeWireCore *core;
-  struct spa_hook core_listener;
-  int last_seq;
-  int pending_seq;
-
-  struct pw_stream *stream;
-  struct spa_hook stream_listener;
-
   GstBuffer *last_buffer;
-  GstStructure *stream_properties;
-
-  GstPipeWirePool *pool;
-  GstClock *clock;
-  GstClockTime last_time;
 
   enum spa_meta_videotransform_value transform_value;
 };

src/gst/gstpipewirestream.h
@@ -1,0 +1,53 @@
+/* GStreamer */
+/* SPDX-FileCopyrightText: Copyright © 2018 Wim Taymans */
+/* SPDX-FileCopyrightText: Copyright © 2024 Collabora Ltd. */
+/* SPDX-License-Identifier: MIT */
+
+#ifndef __GST_PIPEWIRE_STREAM_H__
+#define __GST_PIPEWIRE_STREAM_H__
+
+#include "config.h"
+
+#include "gstpipewirecore.h"
+#include "gstpipewirepool.h"
+#include "gstpipewireclock.h"
+
+#include <gst/gst.h>
+#include <pipewire/pipewire.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_PIPEWIRE_STREAM (gst_pipewire_stream_get_type())
+G_DECLARE_FINAL_TYPE(GstPipeWireStream, gst_pipewire_stream, GST, PIPEWIRE_STREAM, GstObject)
+
+struct _GstPipeWireStream {
+  GstObject parent;
+
+  /* relatives */
+  GstElement *element;
+  GstPipeWireCore *core;
+  GstPipeWirePool *pool;
+  GstClock *clock;
+
+  /* the actual pw stream */
+  struct pw_stream *pwstream;
+  struct spa_hook pwstream_listener;
+
+  /* common properties */
+  int fd;
+  gchar *path;
+  gchar *target_object;
+  gchar *client_name;
+  GstStructure *client_properties;
+  GstStructure *stream_properties;
+};
+
+GstPipeWireStream * gst_pipewire_stream_new (GstElement * element);
+
+gboolean gst_pipewire_stream_open (GstPipeWireStream * self,
+    const struct pw_stream_events * pwstream_events);
+void gst_pipewire_stream_close (GstPipeWireStream * self);
+
+G_END_DECLS
+
+#endif /* __GST_PIPEWIRE_STREAM_H__ */

src/gst/meson.build
@@ -6,6 +6,7 @@ pipewire_gst_sources = [
   'gstpipewirepool.c',
   'gstpipewiresink.c',
   'gstpipewiresrc.c',
+  'gstpipewirestream.c',
 ]
 
 if get_option('gstreamer-device-provider').allowed()
@@ -20,6 +21,7 @@ pipewire_gst_headers = [
   'gstpipewirepool.h',
   'gstpipewiresink.h',
   'gstpipewiresrc.h',
+  'gstpipewirestream.h',
 ]
 
 pipewire_gst = shared_library('gstpipewire',

commit 586e116f3cd0725c9c152aa7e9d6483327582f52

gst: stream: Destroy stream before clearing variable
`pw_stream_destroy()` chains up to `on_remove_buffer()` in
GstPipeWireSrc which again needs the stream to still be around to call
`pw_stream_queue_buffer()` on it. By using `g_clear_pointer()` it will
already have been cleared, causing crashes.

Revert to the behavior from before the commit mentioned below and add a
comment in order to avoid regressing in a future cleanup.

Fixes: 0c40c0147 (gst: factor out the stream management and some common variables in a new class)

src/gst/gstpipewirestream.c
@@ -158,7 +158,12 @@ gst_pipewire_stream_close (GstPipeWireStream * self)
 
   /* destroy the pw stream */
   pw_thread_loop_lock (self->core->loop);
-  g_clear_pointer (&self->pwstream, pw_stream_destroy);
+  if (self->pwstream) {
+     /* Do not use g_clear_pointer() here as pw_stream_destroy() may chain up to
+      * code requiring the pointer to still be around */
+     pw_stream_destroy (self->pwstream);
+     self->pwstream = NULL;
+  }
   pw_thread_loop_unlock (self->core->loop);
 
   /* release the core */
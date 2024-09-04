22bdd52db5c9f85bde1afa0c9e74f69aa97ada75
Wim Taymans <wtaymans@redhat.com>
2024-09-04 18:05:29 +0200

    global: avoid recursive destroy calls
    Fixes #4250

--- src/pipewire/global.c
+++ src/pipewire/global.c
@@ -390,6 +390,9 @@ void pw_global_destroy(struct pw_global *global)
 	struct pw_resource *resource;
 	struct pw_context *context = global->context;
 
+	if (global->destroyed)
+		return;
+
 	global->destroyed = true;
 
 	pw_log_debug("%p: destroy %u", global, global->id);

67ddfc3053f383af4436d1a1a7fbcbc7e4136547
Gleb Popov <6yearold@gmail.com>
2024-09-15 16:48:25 +0300

    Use the 'thrd_success' constant when checking for tss_create result

--- spa/plugins/support/loop.c
+++ spa/plugins/support/loop.c
@@ -1171,7 +1171,7 @@ impl_init(const struct spa_handle_factory *factory,
 		goto error_exit_free_poll;
 	}
 
-	if (tss_create(&impl->queue_tss_id, (tss_dtor_t)loop_queue_destroy) != 0) {
+	if (tss_create(&impl->queue_tss_id, (tss_dtor_t)loop_queue_destroy) != thrd_success) {
 		res = -errno;
 		spa_log_error(impl->log, "%p: can't create tss: %m", impl);
 		goto error_exit_free_wakeup;

PipeWire/pipewire

commit f2f204d60458753308792b3adcb86e443d13fe49

module-rtp-sap: Initialise send fd when first needed
Avoids need for additional configuration to allow disabling send (which
in turn was needed to avoid errors when a network interface is not
available on start).

src/modules/module-rtp-sap.c
@@ -45,10 +45,6 @@
  * sess.sap.announce = true and it will create a receiver for all announced
  * streams.
  *
- * If `sap.announcer` is set to `false`, the announce-stream action will not
- * work. Use this to avoid opening a send socket if you are not going to use
- * this functionality.
- *
  * ## Module Name
  *
  * `libpipewire-module-rtp-sap`
@@ -68,7 +64,6 @@
  * - `sap.max-sessions = <int>`: maximum number of concurrent send/receive sessions to track
  * - `sap.preamble-extra = [strings]`: extra attributes to add to the atomic SDP preamble
  * - `sap.end-extra = [strings]`: extra attributes to add to the end of the SDP message
- * - `sap.announcer = <bool>`: enable announcing local streams, default true
  *
  * ## General options
  *
@@ -270,7 +265,6 @@ struct impl {
 
 	struct sockaddr_storage src_addr;
 	socklen_t src_len;
-	bool announcer;
 
 	uint16_t sap_port;
 	struct sockaddr_storage sap_addr;
@@ -856,6 +850,49 @@ static struct session *session_new_announce(struct impl *impl, struct node *node
 		return NULL;
 	}
 
+	if (impl->sap_fd == -1) {
+		int fd;
+		char addr[64];
+
+		if ((str = pw_properties_get(props, "source.ip")) == NULL) {
+			if (impl->ifname) {
+				int fd = socket(impl->sap_addr.ss_family, SOCK_DGRAM, 0);
+				if (fd >= 0) {
+					struct ifreq req;
+					spa_zero(req);
+					req.ifr_addr.sa_family = impl->sap_addr.ss_family;
+					snprintf(req.ifr_name, sizeof(req.ifr_name), "%s", impl->ifname);
+					res = ioctl(fd, SIOCGIFADDR, &req);
+					if (res < 0)
+						pw_log_warn("SIOCGIFADDR %s failed: %m", impl->ifname);
+					str = inet_ntop(req.ifr_addr.sa_family,
+							&((struct sockaddr_in *)&req.ifr_addr)->sin_addr,
+							addr, sizeof(addr));
+					if (str == NULL) {
+						pw_log_warn("can't parse interface ip: %m");
+					} else {
+						pw_log_info("interface %s IP: %s", impl->ifname, str);
+					}
+					close(fd);
+				}
+			}
+			if (str == NULL)
+				str = impl->sap_addr.ss_family == AF_INET ?
+					DEFAULT_SOURCE_IP : DEFAULT_SOURCE_IP6;
+		}
+		if ((res = pw_net_parse_address(str, 0, &impl->src_addr, &impl->src_len)) < 0) {
+			pw_log_error("invalid source.ip %s: %s", str, spa_strerror(res));
+			return NULL;
+		}
+
+		if ((fd = make_send_socket(&impl->src_addr, impl->src_len,
+						&impl->sap_addr, impl->sap_len,
+						impl->mcast_loop, impl->ttl)) < 0)
+			return NULL;
+
+		impl->sap_fd = fd;
+	}
+
 	sess = calloc(1, sizeof(struct session));
 	if (sess == NULL)
 		return NULL;
@@ -1091,7 +1128,7 @@ static int rule_matched(void *data, const char *location, const char *action,
 
 		session_load_source(i->session, i->props);
 	}
-	else if (i->node && i->impl->announcer && spa_streq(action, "announce-stream")) {
+	else if (i->node && spa_streq(action, "announce-stream")) {
 		struct pw_properties *props;
 
 		if ((props = pw_properties_new_dict(i->node->info->props)) == NULL)
@@ -1492,15 +1529,6 @@ static int start_sap(struct impl *impl)
 	struct timespec value, interval;
 	char addr[128] = "invalid";
 
-	if (impl->announcer) {
-		if ((fd = make_send_socket(&impl->src_addr, impl->src_len,
-						&impl->sap_addr, impl->sap_len,
-						impl->mcast_loop, impl->ttl)) < 0)
-			return fd;
-
-		impl->sap_fd = fd;
-	}
-
 	pw_log_info("starting SAP timer");
 	impl->timer = pw_loop_add_timer(impl->loop, on_timer_event, impl);
 	if (impl->timer == NULL) {
@@ -1717,7 +1745,6 @@ int pipewire__module_init(struct pw_impl_module *module, const char *args)
 	uint32_t port;
 	const char *str;
 	int res = 0;
-	char addr[64];
 
 	PW_LOG_TOPIC_INIT(mod_topic);
 
@@ -1742,8 +1769,6 @@ int pipewire__module_init(struct pw_impl_module *module, const char *args)
 	impl->module = module;
 	impl->loop = pw_context_get_main_loop(context);
 
-	impl->announcer = pw_properties_get_bool(props, "sap.announcer", true);
-
 	str = pw_properties_get(props, "local.ifname");
 	impl->ifname = str ? strdup(str) : NULL;
 
@@ -1763,37 +1788,6 @@ int pipewire__module_init(struct pw_impl_module *module, const char *args)
 	}
 	impl->cleanup_interval = pw_properties_get_uint32(impl->props,
 			"sap.cleanup.sec", DEFAULT_CLEANUP_SEC);
-
-	if ((str = pw_properties_get(props, "source.ip")) == NULL) {
-		if (impl->ifname && impl->announcer) {
-			int fd = socket(impl->sap_addr.ss_family, SOCK_DGRAM, 0);
-			if (fd >= 0) {
-				struct ifreq req;
-				spa_zero(req);
-				req.ifr_addr.sa_family = impl->sap_addr.ss_family;
-				snprintf(req.ifr_name, sizeof(req.ifr_name), "%s", impl->ifname);
-				res = ioctl(fd, SIOCGIFADDR, &req);
-				if (res < 0)
-					pw_log_warn("SIOCGIFADDR %s failed: %m", impl->ifname);
-				str = inet_ntop(req.ifr_addr.sa_family,
-						&((struct sockaddr_in *)&req.ifr_addr)->sin_addr,
-						addr, sizeof(addr));
-				if (str == NULL) {
-					pw_log_warn("can't parse interface ip: %m");
-				} else {
-					pw_log_info("interface %s IP: %s", impl->ifname, str);
-				}
-				close(fd);
-			}
-		}
-		if (str == NULL)
-			str = impl->sap_addr.ss_family == AF_INET ?
-				DEFAULT_SOURCE_IP : DEFAULT_SOURCE_IP6;
-	}
-	if ((res = pw_net_parse_address(str, 0, &impl->src_addr, &impl->src_len)) < 0) {
-		pw_log_error("invalid source.ip %s: %s", str, spa_strerror(res));
-		goto out;
-	}
 
 	impl->ttl = pw_properties_get_uint32(props, "net.ttl", DEFAULT_TTL);
 	impl->mcast_loop = pw_properties_get_bool(props, "net.loop", DEFAULT_LOOP);

commit fbbe983a05dcddb51751e6762e099e399fa479bc

rtp-sap: make the receive socket listen only to the configured sap.ip
In line also with da8e207de9d14118e759865a6fd418d4edacf4ae,
make sure that the rtp-sap receive socket listens only to the configured
sap.ip in unicast mode, instead of accepting all packets on the port.

This additionally fixes breakage that was inadvertently introduced in
f2f204d60458753308792b3adcb86e443d13fe49. Because the
`struct sockaddr_storage *sa` argument in `make_recv_socket` points
to `impl->sap_addr`, changing the address to INADDR_ANY in the unicast
code path would also silently cause the sender socket to try to
connect to INADDR_ANY and fail.

src/modules/module-rtp-sap.c
@@ -455,6 +455,8 @@ static int make_recv_socket(struct sockaddr_storage *sa, socklen_t salen,
 {
 	int af, fd, val, res;
 	struct ifreq req;
+	struct sockaddr_storage ba = *sa;
+	bool do_connect = false;
 	char addr[128];
 
 	af = sa->ss_family;
@@ -488,7 +490,11 @@ static int make_recv_socket(struct sockaddr_storage *sa, socklen_t salen,
 			pw_log_info("join IPv4 group: %s iface:%d", addr, req.ifr_ifindex);
 			res = setsockopt(fd, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mr4, sizeof(mr4));
 		} else {
-			sa4->sin_addr.s_addr = INADDR_ANY;
+			struct sockaddr_in *ba4 = (struct sockaddr_in*)&ba;
+			if (ba4->sin_addr.s_addr != INADDR_ANY) {
+				ba4->sin_addr.s_addr = INADDR_ANY;
+				do_connect = true;
+			}
 		}
 	} else if (af == AF_INET6) {
 		struct sockaddr_in6 *sa6 = (struct sockaddr_in6*)sa;
@@ -501,7 +507,8 @@ static int make_recv_socket(struct sockaddr_storage *sa, socklen_t salen,
 			pw_log_info("join IPv6 group: %s iface:%d", addr, req.ifr_ifindex);
 			res = setsockopt(fd, IPPROTO_IPV6, IPV6_JOIN_GROUP, &mr6, sizeof(mr6));
 		} else {
-		        sa6->sin6_addr = in6addr_any;
+			struct sockaddr_in6 *ba6 = (struct sockaddr_in6*)&ba;
+			ba6->sin6_addr = in6addr_any;
 		}
 	} else {
 		res = -EINVAL;
@@ -514,10 +521,17 @@ static int make_recv_socket(struct sockaddr_storage *sa, socklen_t salen,
 		goto error;
 	}
 
-	if (bind(fd, (struct sockaddr*)sa, salen) < 0) {
+	if (bind(fd, (struct sockaddr*)&ba, salen) < 0) {
 		res = -errno;
 		pw_log_error("bind() failed: %m");
 		goto error;
+	}
+	if (do_connect) {
+		if (connect(fd, (struct sockaddr*)sa, salen) < 0) {
+			res = -errno;
+			pw_log_error("connect() failed: %m");
+			goto error;
+		}
 	}
 	return fd;
 error:
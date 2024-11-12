PipeWire/pipewire

commit e2991f6398625059538e4d99d6b0553d3a0f5968

json: add helper function to parse channel positions
Use the helper instead of duplicating the same code.

Also add some helpers to parse a json array of uint32_t

Move some functions to convert between type name and id.

src/modules/module-protocol-pulse/format.c
@@ -7,6 +7,7 @@
 #include <spa/param/audio/format.h>
 #include <spa/param/audio/format-utils.h>
 #include <spa/param/audio/raw.h>
+#include <spa/param/audio/raw-json.h>
 #include <spa/utils/json.h>
 
 #include "format.h"
@@ -130,22 +131,12 @@ uint32_t format_pa2id(enum sample_format format)
 
 const char *format_id2name(uint32_t format)
 {
-	int i;
-	for (i = 0; spa_type_audio_format[i].name; i++) {
-		if (spa_type_audio_format[i].type == format)
-			return spa_debug_type_short_name(spa_type_audio_format[i].name);
-	}
-	return "UNKNOWN";
+	return spa_type_audio_format_to_short_name(format);
 }
 
 uint32_t format_name2id(const char *name)
 {
-	int i;
-	for (i = 0; spa_type_audio_format[i].name; i++) {
-		if (spa_streq(name, spa_debug_type_short_name(spa_type_audio_format[i].name)))
-			return spa_type_audio_format[i].type;
-	}
-	return SPA_AUDIO_FORMAT_UNKNOWN;
+	return spa_type_audio_format_from_short_name(name);
 }
 
 uint32_t format_paname2id(const char *name, size_t size)
@@ -289,22 +280,12 @@ uint32_t channel_pa2id(enum channel_position channel)
 
 const char *channel_id2name(uint32_t channel)
 {
-	int i;
-	for (i = 0; spa_type_audio_channel[i].name; i++) {
-		if (spa_type_audio_channel[i].type == channel)
-			return spa_debug_type_short_name(spa_type_audio_channel[i].name);
-	}
-	return "UNK";
+	return spa_type_audio_channel_to_short_name(channel);
 }
 
 uint32_t channel_name2id(const char *name)
 {
-	int i;
-	for (i = 0; spa_type_audio_channel[i].name; i++) {
-		if (strcmp(name, spa_debug_type_short_name(spa_type_audio_channel[i].name)) == 0)
-			return spa_type_audio_channel[i].type;
-	}
-	return SPA_AUDIO_CHANNEL_UNKNOWN;
+	return spa_type_audio_channel_from_short_name(name);
 }
 
 enum channel_position channel_id2pa(uint32_t id, uint32_t *aux)
@@ -352,6 +333,13 @@ void channel_map_to_positions(const struct channel_map *map, uint32_t *pos)
 	int i;
 	for (i = 0; i < map->channels; i++)
 		pos[i] = map->map[i];
+}
+
+void positions_to_channel_map(const uint32_t *pos, uint32_t channels, struct channel_map *map)
+{
+	uint32_t i;
+	for (i = 0; i < channels; i++)
+		map->map[i] = pos[i];
 }
 
 void channel_map_parse(const char *str, struct channel_map *map)
@@ -440,17 +428,9 @@ void channel_map_parse(const char *str, struct channel_map *map)
 
 void channel_map_parse_position(const char *str, struct channel_map *map)
 {
-	struct spa_json it[1];
-	char v[256];
-
-	if (spa_json_begin_array_relax(&it[0], str, strlen(str)) <= 0)
-		return;
-
-	map->channels = 0;
-	while (spa_json_get_string(&it[0], v, sizeof(v)) > 0 &&
-	    map->channels < SPA_AUDIO_MAX_CHANNELS) {
-		map->map[map->channels++] = channel_name2id(v);
-	}
+	uint32_t channels = 0, position[SPA_AUDIO_MAX_CHANNELS];
+	spa_audio_parse_position(str, strlen(str), position, &channels);
+	positions_to_channel_map(position, channels, map);
 }
 
 bool channel_map_valid(const struct channel_map *map)

pipewire-alsa/alsa-plugins/pcm_pipewire.c
@@ -772,7 +772,7 @@ static int snd_pcm_pipewire_hw_params(snd_pcm_ioplug_t * io,
 		pw->requested.info.raw.channels = io->channels;
 		pw->requested.info.raw.rate = io->rate;
 		set_default_channels(io->channels, pw->requested.info.raw.position);
-		fmt_str = spa_debug_type_find_name(spa_type_audio_format, pw->requested.info.raw.format);
+		fmt_str = spa_type_audio_format_to_short_name(pw->requested.info.raw.format);
 		pw->format = pw->requested;
 		break;
 	case SPA_MEDIA_SUBTYPE_dsd:

spa/include/spa/param/audio/iec958-types.h
@@ -33,6 +33,14 @@ static const struct spa_type_info spa_type_audio_iec958_codec[] = {
 	{ 0, 0, NULL, NULL },
 };
 
+static inline uint32_t spa_type_audio_iec958_codec_from_short_name(const char *name)
+{
+	return spa_type_from_short_name(name, spa_type_audio_iec958_codec, SPA_AUDIO_IEC958_CODEC_UNKNOWN);
+}
+static inline const char * spa_type_audio_iec958_codec_to_short_name(uint32_t type)
+{
+	return spa_type_to_short_name(type, spa_type_audio_iec958_codec, "UNKNOWN");
+}
 /**
  * \}
  */

spa/include/spa/param/audio/raw-json.h
@@ -1,0 +1,47 @@
+/* Simple Plugin API */
+/* SPDX-FileCopyrightText: Copyright © 2024 Wim Taymans */
+/* SPDX-License-Identifier: MIT */
+
+#ifndef SPA_AUDIO_RAW_JSON_H
+#define SPA_AUDIO_RAW_JSON_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \addtogroup spa_param
+ * \{
+ */
+
+#include <spa/utils/json.h>
+#include <spa/param/audio/raw-types.h>
+
+static inline int
+spa_audio_parse_position(const char *str, size_t len,
+		uint32_t *position, uint32_t *n_channels)
+{
+	struct spa_json iter;
+        char v[256];
+	uint32_t channels = 0;
+
+        if (spa_json_begin_array_relax(&iter, str, len) <= 0)
+                return 0;
+
+        while (spa_json_get_string(&iter, v, sizeof(v)) > 0 &&
+		channels < SPA_AUDIO_MAX_CHANNELS) {
+                position[channels++] = spa_type_audio_channel_from_short_name(v);
+        }
+	*n_channels = channels;
+	return channels;
+}
+
+/**
+ * \}
+ */
+
+#ifdef __cplusplus
+}  /* extern "C" */
+#endif
+
+#endif /* SPA_AUDIO_RAW_JSON_H */

spa/include/spa/param/audio/raw-types.h
@@ -129,6 +129,15 @@ static const struct spa_type_info spa_type_audio_format[] = {
 	{ 0, 0, NULL, NULL },
 };
 
+static inline uint32_t spa_type_audio_format_from_short_name(const char *name)
+{
+	return spa_type_from_short_name(name, spa_type_audio_format, SPA_AUDIO_FORMAT_UNKNOWN);
+}
+static inline const char * spa_type_audio_format_to_short_name(uint32_t type)
+{
+	return spa_type_to_short_name(type, spa_type_audio_format, "UNKNOWN");
+}
+
 #define SPA_TYPE_INFO_AudioFlags	SPA_TYPE_INFO_FLAGS_BASE "AudioFlags"
 #define SPA_TYPE_INFO_AUDIO_FLAGS_BASE	SPA_TYPE_INFO_AudioFlags ":"
 
@@ -250,12 +259,11 @@ static const struct spa_type_info spa_type_audio_channel[] = {
 
 static inline uint32_t spa_type_audio_channel_from_short_name(const char *name)
 {
-	int i;
-	for (i = 0; spa_type_audio_channel[i].name; i++) {
-		if (spa_streq(name, spa_type_short_name(spa_type_audio_channel[i].name)))
-			return spa_type_audio_channel[i].type;
-	}
-	return SPA_AUDIO_CHANNEL_UNKNOWN;
+	return spa_type_from_short_name(name, spa_type_audio_channel, SPA_AUDIO_CHANNEL_UNKNOWN);
+}
+static inline const char * spa_type_audio_channel_to_short_name(uint32_t type)
+{
+	return spa_type_to_short_name(type, spa_type_audio_channel, "UNK");
 }
 
 

spa/include/spa/param/video/raw-types.h
@@ -20,7 +20,8 @@ extern "C" {
 #define SPA_TYPE_INFO_VIDEO_FORMAT_BASE		SPA_TYPE_INFO_VideoFormat ":"
 
 static const struct spa_type_info spa_type_video_format[] = {
-	{ SPA_VIDEO_FORMAT_ENCODED,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "encoded", NULL },
+	{ SPA_VIDEO_FORMAT_UNKNOWN,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "UNKNOWN", NULL },
+	{ SPA_VIDEO_FORMAT_ENCODED,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "ENCODED", NULL },
 	{ SPA_VIDEO_FORMAT_I420,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "I420", NULL },
 	{ SPA_VIDEO_FORMAT_YV12,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "YV12", NULL },
 	{ SPA_VIDEO_FORMAT_YUY2,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "YUY2", NULL },
@@ -109,6 +110,15 @@ static const struct spa_type_info spa_type_video_format[] = {
 	{ SPA_VIDEO_FORMAT_BGRA_102LE,	SPA_TYPE_Int, SPA_TYPE_INFO_VIDEO_FORMAT_BASE "BGRA_102LE", NULL },
 	{ 0, 0, NULL, NULL },
 };
+
+static inline uint32_t spa_type_video_format_from_short_name(const char *name)
+{
+	return spa_type_from_short_name(name, spa_type_video_format, SPA_VIDEO_FORMAT_UNKNOWN);
+}
+static inline const char * spa_type_video_format_to_short_name(uint32_t type)
+{
+	return spa_type_to_short_name(type, spa_type_video_format, "UNKNOWN");
+}
 
 #define SPA_TYPE_INFO_VideoFlags	SPA_TYPE_INFO_FLAGS_BASE "VideoFlags"
 #define SPA_TYPE_INFO_VIDEO_FLAGS_BASE	SPA_TYPE_INFO_VideoFlags ":"

spa/include/spa/utils/json.h
@@ -185,6 +185,24 @@ static inline int spa_json_begin_array(struct spa_json * iter, const char *data,
 	return spa_json_begin_container(iter, data, size, '[', false);
 }
 
+#define spa_json_make_str_array_unpack(maxlen,type,conv)			\
+{										\
+	struct spa_json iter;							\
+	char v[maxlen];								\
+	uint32_t count = 0;							\
+        if (spa_json_begin_array_relax(&iter, arr, arr_len) <= 0)		\
+		return -EINVAL;							\
+	while (spa_json_get_string(&iter, v, sizeof(v)) > 0 && count < max)	\
+		values[count++] = conv(v);					\
+	return count;								\
+}
+
+static inline int spa_json_str_array_uint32(const char *arr, size_t arr_len,
+		uint32_t *values, size_t max)
+{
+	spa_json_make_str_array_unpack(32,uint32_t, atoi);
+}
+
 /**
  * \}
  */

spa/include/spa/utils/type.h
@@ -10,6 +10,7 @@ extern "C" {
 #endif
 
 #include <spa/utils/defs.h>
+#include <spa/utils/string.h>
 
 /** \defgroup spa_types Types
  * Data type information enumerations
@@ -134,6 +135,34 @@ static inline const char *spa_type_short_name(const char *name)
 	if ((h = strrchr(name, ':')) != NULL)
 		name = h + 1;
 	return name;
+}
+
+static inline uint32_t spa_type_from_short_name(const char *name,
+		const struct spa_type_info *info, uint32_t unknown)
+{
+	int i;
+	for (i = 0; info[i].name; i++) {
+		if (spa_streq(name, spa_type_short_name(info[i].name)))
+			return info[i].type;
+	}
+	return unknown;
+}
+static inline const char * spa_type_to_name(uint32_t type,
+		const struct spa_type_info *info, const char *unknown)
+{
+	int i;
+	for (i = 0; info[i].name; i++) {
+		if (info[i].type == type)
+			return info[i].name;
+	}
+	return unknown;
+}
+
+static inline const char * spa_type_to_short_name(uint32_t type,
+		const struct spa_type_info *info, const char *unknown)
+{
+	const char *n = spa_type_to_name(type, info, unknown);
+	return n ? spa_type_short_name(n) : NULL;
 }
 
 /**

commit c8c89f7517e6cfb81a2029868c0d0f443e023134

pulse: also set channels in the map
When converting a position to a channel-map, copy the number of channels
as well.

This broke with commit e2991f6398625059538e4d99d6b0553d3a0f5968

src/modules/module-protocol-pulse/format.c
@@ -340,6 +340,7 @@ void positions_to_channel_map(const uint32_t *pos, uint32_t channels, struct cha
 	uint32_t i;
 	for (i = 0; i < channels; i++)
 		map->map[i] = pos[i];
+	map->channels = channels;
 }
 
 void channel_map_parse(const char *str, struct channel_map *map)
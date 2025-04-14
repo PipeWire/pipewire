514f0ccd9c43dfe7c60eab405c214a498d0a7553
hackerman-kl <hakerman-kl@kebag-logic.com>
2025-04-14 18:27:19 +0000

    milan: adding banner to the unsol notification helper

--- src/modules/module-avb/aecp-cmd-resp/aecp-aem-unsol-helper.h
+++ src/modules/module-avb/aecp-cmd-resp/aecp-aem-unsol-helper.h
@@ -1,3 +1,8 @@
+/* AVB support */
+/* SPDX-FileCopyrightText: Copyright © 2025 Kebag-Logic */
+/* SPDX-FileCopyrightText: Copyright © 2025 Alex Malki <alexandre.malki@kebag-logic.com> */
+/* SPDX-License-Identifier: MIT  */
+
 #ifndef __AECP_AEM_UNSOL_HELPER_H__
 #define __AECP_AEM_UNSOL_HELPER_H__
 

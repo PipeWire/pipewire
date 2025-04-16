1f27a075fa74d41596370e2d2ddfe672a7cb4700
Simon Gapp <simongapp@gmx.de>
2025-04-16 21:29:50 +0000

    #29: using the pipewire-helper repo now

--- src/modules/module-avb/aecp-aem-controls.h
+++ src/modules/module-avb/aecp-aem-controls.h
@@ -1,13 +1,14 @@
 /* AVB support */
 /* SPDX-FileCopyrightText: Copyright © 2025 Kebag-Logic */
 /* SPDX-FileCopyrightText: Copyright © 2025 Alexandre Malki <alexandre.malki@kebag-logic.com> */
+/* SPDX-FileCopyrightText: Copyright © 2025 Simon Gapp <simon.gapp@kebag-logic.com> */
 /* SPDX-License-Identifier: MIT */
 
 #ifndef __AECP_AEM_CONTROLS_H__
 #define __AECP_AEM_CONTROLS_H__
 
 // TODO, When all the AVB needs to be supported then addition needs to be don here
-
+/* IEEE 1722.1-2021, Table 7-121 - Value Types*/
 #define AECP_AEM_CTRL_LINEAR_INT8             0x0000
 #define AECP_AEM_CTRL_LINEAR_UINT8            0x0001
 #define AECP_AEM_CTRL_LINEAR_INT16            0x0002
@@ -51,14 +52,21 @@
 #define AECP_AEM_CTRL_CTRL_VENDOR          0x3ffe
 
 /* Definition of the UNIT codes */
+/* IEEE 1722.1-2021, Table 7-75 - Codes for Unitless quantities*/
 #define AECP_AEM_CTRL_UNIT_CODE_UNITLESS      (0)
+#define AECP_AEM_CTRL_UNIT_CODE_COUNT         (1)
+#define AECP_AEM_CTRL_UNIT_CODE_PERCENT       (2)
+#define AECP_AEM_CTRL_UNIT_CODE_FSTOP         (3)       
 
 
 
 #define AECP_AEM_CTRL_FORMAT_VENDOR           (0)
 #define AECP_AEM_CTRL_FORMAT_AVDECC           (1)
 
-/* Identify 1722.1 7.3.5.2 Also defined in the Milan V1.2 - 5.4.5.4   */
+/* Identify
+* IEEE 1722.1, Sec. 7.3.5.2 - Identify Control (IDENTIFY)
+* Milan v1.2, Sec. 5.4.5.4 - Identification notification  
+*/
 #define AECP_AEM_CTRL_IDENTIFY_UNIT_MULTIPLY  0
 #define AECP_AEM_CTRL_IDENTIFY_UNIT_CODE      AECP_AEM_CTRL_UNIT_CODE_UNITLESS
 #define AECP_AEM_CTRL_IDENTIFY_STEP           (255)
@@ -66,8 +74,8 @@
 #define AECP_AEM_CTRL_IDENTIFY_MAXIMUM        (255)
 
 
-#define BASE_CTRL_TYPE_MAC            { 0x91, 0xe0, 0xf0, 0x01, 0x00, 0x00 };
-// TODO AVB, for now support limited to the Milan
-#define BASE_CTRL_IDENTIFY_MAC        { 0x91, 0xe0, 0xf0, 0x00, 0x00, 0x01 };
+#define BASE_CTRL_TYPE_MAC            { 0x90, 0xe0, 0xf0, 0x01, 0x00, 0x00 };
+// TODO AVB, for now support limited to Milan
+#define BASE_CTRL_IDENTIFY_MAC        { 0x90, 0xe0, 0xf0, 0x00, 0x00, 0x01 };
 
 #endif //__AECP_AEM_CONTROLS_H__

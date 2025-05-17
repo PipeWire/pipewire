PipeWire/pipewire

commit bcf0c0cf89f680078dcfdd70f3561855cfe880b7

v4l2: only skip buffer for raw formats
We don't want to skip the first buffer for encoded formats because it
can contain the encoding parameters, in the h264 case, for example.

spa/plugins/v4l2/v4l2-utils.c
@@ -1873,7 +1873,10 @@ static int spa_v4l2_stream_on(struct impl *this)
 
 	spa_log_debug(this->log, "starting");
 
-	port->first_buffer = true;
+	if (port->current_format.media_subtype == SPA_MEDIA_SUBTYPE_raw)
+		port->first_buffer = true;
+	else
+		port->first_buffer = false;
 	mmap_read(this);
 
 	type = V4L2_BUF_TYPE_VIDEO_CAPTURE;

commit e387772dc2a4316d88d41b528b6b2b16eccf32ba

v4l2: Re-enable first buffer skip for jpeg types
The workaround is typically needed with usb-cameras using jpeg streams.
Re-enabling the skip shouldn't affect what the commit was trying to do,
i.e. fix encoded streams like h264 etc.

Fixes: bcf0c0cf8 (v4l2: only skip buffer for raw formats)

spa/plugins/v4l2/v4l2-utils.c
@@ -1873,7 +1873,9 @@ static int spa_v4l2_stream_on(struct impl *this)
 
 	spa_log_debug(this->log, "starting");
 
-	if (port->current_format.media_subtype == SPA_MEDIA_SUBTYPE_raw)
+	if (port->current_format.media_subtype == SPA_MEDIA_SUBTYPE_raw ||
+	    port->current_format.media_subtype == SPA_MEDIA_SUBTYPE_mjpg ||
+	    port->current_format.media_subtype == SPA_MEDIA_SUBTYPE_jpeg)
 		port->first_buffer = true;
 	else
 		port->first_buffer = false;
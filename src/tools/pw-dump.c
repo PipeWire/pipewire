487c7ee93319384117a94f542c60ebc1dae152a4
Arun Raghavan <arun@asymptotic.io>
2025-08-25 09:48:19 -0400

    tools: Fix -C handling for pw-dump
    The short form needs to have a :: to signal an optional argument so
    that something like -Calways can work.

--- src/tools/pw-dump.c
+++ src/tools/pw-dump.c
@@ -1541,7 +1541,7 @@ int main(int argc, char *argv[])
 		colors = true;
 	setlinebuf(data.out);
 
-	while ((c = getopt_long(argc, argv, "hVr:mNC", long_options, NULL)) != -1) {
+	while ((c = getopt_long(argc, argv, "hVr:mNC::", long_options, NULL)) != -1) {
 		switch (c) {
 		case 'h' :
 			show_help(&data, argv[0], false);

PipeWire/pipewire

commit 77143e54d96070f963a8a78cf58322d1afe537b9

gstpipewiresrc: Fix re-linking for audio
For a pipeline like below, we might want to dynamically switch the audio
source.

gst-launch-1.0 -e pipewiresrc autoconnect=false ! queue ! audioconvert ! autoaudiosink

On switching to a different audio source, any one of driver, quantum
or clock rate might change which changes the return `result` value of
gst_pipewire_clock_get_internal_time.

This can result in the basesrc create function incorrectly waiting in
gst_clock_id_wait. We post clock lost message to fix this. In the case
of gst-launch, it will set the pipeline to PAUSED and then PLAYING to
to force a new clock and a new base_time distribution.

Without the clock lost message, the following can be seen

before re-linking to a different source
0:00:30.887602864 79499 0x7fffe8000d40 DEBUG              GST_CLOCK gstsystemclock.c:1158:gst_system_clock_id_wait_jitter_unlocked:<pipewireclock0> entry 0x7fffd803fad0 time 0:00:17.024565416 now 0:00:17.024109144 diff (time-now) 456272

after re-linking to a different source
0:00:45.790843245 79499 0x7fffe8000d40 DEBUG              GST_CLOCK gstsystemclock.c:1158:gst_system_clock_id_wait_jitter_unlocked:<pipewireclock0> entry 0x7fffd803fad0 time 0:00:31.927694059 now 0:00:17.066883864 diff (time-now) 14860810195

With the clock lost message, the following can be seen

before re-linking to a different source
0:01:09.336533552 89461 0x7fffe8000d40 DEBUG              GST_CLOCK gstsystemclock.c:1158:gst_system_clock_id_wait_jitter_unlocked:<pipewireclock0> entry 0x7fffd803fad0 time 0:00:58.198536772 now 0:00:58.197444926 diff (time-now) 1091846

after re-linking to a different source
0:01:21.659827958 89461 0x7fffe8000d40 DEBUG              GST_CLOCK gstsystemclock.c:1158:gst_system_clock_id_wait_jitter_unlocked:<pipewireclock0> entry 0x7fffd803fad0 time 0:28:24.853517646 now 0:28:24.853527204 diff (time-now) -9558

Note the difference in `time` and `now` fields of the above log message.

src/gst/gstpipewiresrc.c
@@ -739,13 +739,29 @@ on_state_changed (void *data,
                   enum pw_stream_state state, const char *error)
 {
   GstPipeWireSrc *pwsrc = data;
+  GstState current_state = GST_ELEMENT_CAST (pwsrc)->current_state;
 
   GST_DEBUG ("got stream state %s", pw_stream_state_as_string (state));
 
   switch (state) {
     case PW_STREAM_STATE_UNCONNECTED:
     case PW_STREAM_STATE_CONNECTING:
+      break;
     case PW_STREAM_STATE_PAUSED:
+      /*
+       * We may see a driver/quantum/clock rate change on switching audio
+       * sources. The same is not applicable for video.
+       *
+       * We post the clock lost message here to take care of a possible
+       * jump or shift in base_time/clock for the pipeline. Application
+       * must handle the clock lost message in it's bus handler by pausing
+       * the pipeline and then setting it back to playing.
+       */
+      if (current_state == GST_STATE_PLAYING && !pwsrc->is_video)
+        gst_element_post_message (GST_ELEMENT_CAST (pwsrc),
+            gst_message_new_clock_lost (GST_OBJECT_CAST (pwsrc),
+                GST_CLOCK_CAST (pwsrc->stream->clock)));
+      break;
     case PW_STREAM_STATE_STREAMING:
       break;
     case PW_STREAM_STATE_ERROR:

commit fa8b0ba018af386c2c03fff288eb24675f9a3994

pipewiresrc: Fix renegotiation
Some downstream elements trigger re-negotiations when the stream is
running, e.g. due to output window resizing for the below pipeline.

gst-launch-1.0 pipewiresrc ! videoconvert ! video/x-raw ! gtk4paintablesink

This fails due to a side effect introduced by commit 77143e54 with
handle_format_change() setting `is_video` to `false` when param was
NULL. Fix this by explicitly tracking media type and not just video
using a boolean.

src/gst/gstpipewiresrc.c
@@ -769,7 +769,7 @@ static GstBuffer *dequeue_buffer(GstPipeWireSrc *pwsrc)
     GST_BUFFER_DTS (buf) = b->time - pwsrc->delay;
   }
 
-  if (pwsrc->is_video) {
+  if (pwsrc->media_type == SPA_MEDIA_TYPE_video) {
     if (pwsrc->video_info.fps_n) {
       GST_BUFFER_DURATION (buf) = gst_util_uint64_scale (GST_SECOND,
           pwsrc->video_info.fps_d, pwsrc->video_info.fps_n);
@@ -912,7 +912,7 @@ on_state_changed (void *data,
        * must handle the clock lost message in it's bus handler by pausing
        * the pipeline and then setting it back to playing.
        */
-      if (current_state == GST_STATE_PLAYING && !pwsrc->is_video)
+      if (current_state == GST_STATE_PLAYING && pwsrc->media_type == SPA_MEDIA_TYPE_audio)
         gst_element_post_message (GST_ELEMENT_CAST (pwsrc),
             gst_message_new_clock_lost (GST_OBJECT_CAST (pwsrc),
                 GST_CLOCK_CAST (pwsrc->stream->clock)));
@@ -1265,7 +1265,7 @@ handle_format_change (GstPipeWireSrc *pwsrc,
   if (param == NULL) {
     GST_DEBUG_OBJECT (pwsrc, "clear format");
     pwsrc->negotiated = FALSE;
-    pwsrc->is_video = FALSE;
+    pwsrc->media_type = SPA_MEDIA_TYPE_unknown;
     return;
   }
 
@@ -1303,7 +1303,7 @@ handle_format_change (GstPipeWireSrc *pwsrc,
     structure = gst_caps_get_structure (pwsrc->caps, 0);
     if (g_str_has_prefix (gst_structure_get_name (structure), "video/") ||
         g_str_has_prefix (gst_structure_get_name (structure), "image/")) {
-      pwsrc->is_video = TRUE;
+      pwsrc->media_type = SPA_MEDIA_TYPE_video;
 
 #ifdef HAVE_GSTREAMER_DMA_DRM
       if (gst_video_is_dma_drm_caps (pwsrc->caps)) {
@@ -1342,10 +1342,12 @@ handle_format_change (GstPipeWireSrc *pwsrc,
        * application/user */
       if (pwsrc->use_bufferpool != USE_BUFFERPOOL_YES)
         pwsrc->use_bufferpool = USE_BUFFERPOOL_NO;
+
+      pwsrc->media_type = SPA_MEDIA_TYPE_audio;
     }
   } else {
     pwsrc->negotiated = FALSE;
-    pwsrc->is_video = FALSE;
+    pwsrc->media_type = SPA_MEDIA_TYPE_unknown;
     pwsrc->is_rawvideo = FALSE;
   }
 

src/gst/gstpipewiresrc.h
@@ -15,6 +15,7 @@
 #include <gst/video/video.h>
 
 #include <pipewire/pipewire.h>
+#include <spa/param/format.h>
 #include <gst/gstpipewirepool.h>
 #include <gst/gstpipewirecore.h>
 
@@ -63,7 +64,7 @@ struct _GstPipeWireSrc {
   GstCaps *caps;
   GstCaps *possible_caps;
 
-  gboolean is_video;
+  enum spa_media_type media_type;
   gboolean is_rawvideo;
   GstVideoInfo video_info;
 #ifdef HAVE_GSTREAMER_DMA_DRM
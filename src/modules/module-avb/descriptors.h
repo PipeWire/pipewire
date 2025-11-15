55e4c7e4cb2218a4affdb8a710955a18e52941a9
hackerman-kl <hackerman-kl@kebag-logic.com>
2025-11-15 09:38:50 +0100

    module-avb: descriptor: adding todo for simplification

--- src/modules/module-avb/descriptors.h
+++ src/modules/module-avb/descriptors.h
@@ -7,6 +7,22 @@
 #include "aecp-aem-descriptors.h"
 #include "internal.h"
 
+/**
+ * \todo This whole code needs to be re-factore,
+ * 	configuring the entity using such a "HARDCODED"
+ *	header would does not allow an easy way to
+ *	adjust parameters.
+ *
+ *	Especially for the people involved in the project
+ *	and do not have the programming skills to modify
+ *	this file.
+ *
+ * \proposition use a YANG model directly derived from this
+ * 	or use the YAML for simplicity.
+ *
+ * 	Having the YANG would allow directly to know the
+ * 	capabilites/limits of the protocol
+ */
 static inline void init_descriptors(struct server *server)
 {
 	server_add_descriptor(server, AVB_AEM_DESC_STRINGS, 0,

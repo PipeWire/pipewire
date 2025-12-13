c7ebc66e6423c38ad1fad3450ccac1e4f669d698
Mason Remaley <mason@anthropicstudios.com>
2025-12-12 18:13:21 -0800

    Adds explanation to reduce chance of regressing the fix

--- spa/plugins/audioconvert/fmt-ops-ssse3.c
+++ spa/plugins/audioconvert/fmt-ops-ssse3.c
@@ -6,6 +6,7 @@
 
 #include <tmmintrin.h>
 
+// Non static, referenced by `fmt-ops-sse41.c`.
 void
 conv_s24_to_f32d_4s_ssse3(void *data, void * SPA_RESTRICT dst[], const void * SPA_RESTRICT src,
 		uint32_t n_channels, uint32_t n_samples)

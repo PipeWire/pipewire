469929d4f6bad8e5204eead667f0c32d87898392
Siva Mahadevan <me@svmhdvn.name>
2026-03-11 14:20:46 -0400

    pipewire: guard linux-specific RLIMIT_* inside #ifdef __linux__

--- src/pipewire/context.c
+++ src/pipewire/context.c
@@ -353,17 +353,19 @@ static int adjust_rlimits(const struct spa_dict *dict)
 		[RLIMIT_CPU]        = "cpu",
 		[RLIMIT_DATA]       = "data",
 		[RLIMIT_FSIZE]      = "fsize",
-		[RLIMIT_LOCKS]      = "locks",
 		[RLIMIT_MEMLOCK]    = "memlock",
-		[RLIMIT_MSGQUEUE]   = "msgqueue",
-		[RLIMIT_NICE]       = "nice",
 		[RLIMIT_NOFILE]     = "nofile",
 		[RLIMIT_NPROC]      = "nproc",
 		[RLIMIT_RSS]        = "rss",
+		[RLIMIT_STACK]      = "stack",
+#ifdef __linux__
+		[RLIMIT_LOCKS]      = "locks",
+		[RLIMIT_MSGQUEUE]   = "msgqueue",
+		[RLIMIT_NICE]       = "nice",
 		[RLIMIT_RTPRIO]     = "rtprio",
 		[RLIMIT_RTTIME]     = "rttime",
 		[RLIMIT_SIGPENDING] = "sigpending",
-		[RLIMIT_STACK]      = "stack",
+#endif
 	};
 	int res;
 	spa_dict_for_each(it, dict) {

357f27658e00639c771d4a03e7696461980d187e
Siva Mahadevan <me@svmhdvn.name>
2026-03-11 20:10:34 -0400

    spa/utils: remove FreeBSD workaround for bswap*
    Both <endian.h> and <byteswap.h> have the things we need.

--- spa/include/spa/utils/endian.h
+++ spa/include/spa/utils/endian.h
@@ -5,7 +5,7 @@
 #ifndef SPA_ENDIAN_H
 #define SPA_ENDIAN_H
 
-#if defined(__FreeBSD__) || defined(__MidnightBSD__)
+#if defined(__MidnightBSD__)
 #include <sys/endian.h>
 #define bswap_16 bswap16
 #define bswap_32 bswap32

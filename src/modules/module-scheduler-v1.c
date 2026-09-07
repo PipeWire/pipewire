94b3482d24eb7a76a57036739b73f1dfe4625168
Wim Taymans <wtaymans@redhat.com>
2026-09-07 18:41:52 +0200

    module-scheduler: only idle non suspended nodes
    When a follower node is not running, only bring it to IDLE when it was
    not SUSPENDED.
    
    Otherwise we always bring non-running followers to the IDLE state until
    they get suspended again by the session manager (when they are device
    nodes).
    
    See #5392

--- src/modules/module-scheduler-v1.c
+++ src/modules/module-scheduler-v1.c
@@ -959,7 +959,7 @@ again:
 				continue;
 			pw_log_debug("%p: follower %p: active:%d '%s'",
 					context, s, s->active, s->name);
-			ensure_state(s, running, true);
+			ensure_state(s, running, s->info.state != PW_NODE_STATE_SUSPENDED);
 		}
 
 		if (transport != PW_NODE_ACTIVATION_COMMAND_NONE) {

c067d5c9470df76425dae936e9059d6f3fbba194
Wim Taymans <wtaymans@redhat.com>
2026-09-10 15:43:33 +0200

    raop: write TCP packet length into the interleaved header
    The length was ORed into the first ALAC payload word while the interleaved
    header retained a zero length. Write the RTP plus encoded-audio length into
    the TCP header and leave the payload untouched.

--- src/modules/module-raop-sink.c
+++ src/modules/module-raop-sink.c
@@ -508,9 +508,8 @@ static void stream_send_packet(void *data, struct iovec *iov, size_t iovlen)
 		aes_encrypt(impl, dst, len);
 
 	if (impl->protocol == PROTO_TCP) {
-		out[0] |= htonl((uint32_t) len + 12);
-		tcp_pkt[0] = htonl(0x24000000);
-  		out_vec[msg.msg_iovlen++] = (struct iovec) { tcp_pkt, 4 };
+		tcp_pkt[0] = htonl(0x24000000 | (len + 12));
+		out_vec[msg.msg_iovlen++] = (struct iovec) { tcp_pkt, 4 };
 	}
 
 	out_vec[msg.msg_iovlen++] = (struct iovec) { header, 12 };
